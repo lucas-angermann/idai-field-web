acb06469e6109234f8eedec5a6e0ddd7cf64c7ce
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-04-14 14:26:20 +0100

    Add httpoisen dep

--- api/mix.exs
+++ api/mix.exs
@@ -36,7 +36,8 @@ defmodule Api.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.6"}
     ]
   end
 end
