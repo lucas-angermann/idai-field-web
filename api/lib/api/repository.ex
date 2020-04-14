6a63a8cefd665e0e8d44939cc15f1af4d0fb96d6
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-04-14 18:35:01 +0200

    Atomize complete elasticsearch result

--- api/lib/api/repository.ex
+++ api/lib/api/repository.ex
@@ -6,11 +6,8 @@ defmodule Api.Repository do
     case HTTPoison.get("localhost:9200/idai-field/resource/_search") do
 
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        %{ "hits" => %{ "hits" => hits }} = Poison.decode! body
-        hits
-             |> Enum.map(fn hit -> {:ok, result} = Map.fetch(hit, "_source"); result end)
-             |> Enum.map(fn hit -> {:ok, result} = Map.fetch(hit, "resource"); result end)
-             |> Enum.map(&atomize/1)
+        %{ hits: %{ hits: hits }} = atomize(Poison.decode! body)
+        hits |> Enum.map(fn %{ _source: %{ resource: resource } } -> resource end)
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         []
@@ -27,15 +24,15 @@ defmodule Api.Repository do
     case HTTPoison.get("localhost:9200/idai-field/resource/" <> id) do
 
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        %{ "_source" => %{ "resource" => resource }} = Poison.decode! body
-        atomize resource
+        %{ _source: %{ resource: resource }} = atomize Poison.decode! body
+        resource
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        %{"type" => "null", "identifier" => "0"}
+        %{type: "null", identifier: "0"}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
-        %{"type" => "null", "identifier" => "0"}
+        %{type: "null", identifier: "0"}
     end
   end
 end
--- api/lib/utils.ex
+++ api/lib/utils.ex
@@ -1,7 +1,14 @@
 defmodule Utils do
 
   def atomize(map) do
-    for {key, val} <- map, into: %{}, do: {String.to_atom(key), val}
+    for {key, val} <- map, into: %{}, do: {String.to_atom(key),
+      case val do
+        [] -> []
+        list = [head|tail] -> for item <- list, into: [], do: atomize item
+        map = %{} -> atomize map
+        value -> value
+      end
+    }
   end
 
 end
