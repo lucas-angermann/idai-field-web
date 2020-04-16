1925d8a20b4c19f7c2fdaf12e55b22f2337fc643
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-04-16 20:45:51 +0100

    Use $ instead of _

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { compose as _ } from 'tsfun/async';
+import { compose as $ } from 'tsfun/async';
 import SearchBar from './SearchBar';
 import ResultList from './ResultList';
 import {search} from './search';
@@ -10,7 +10,7 @@ export default () => {
     const [results, setResults] = useState([]);
 
     return <div> 
-        <SearchBar onSubmit={_(search, setResults)}></SearchBar>
+        <SearchBar onSubmit={$(search, setResults)}></SearchBar>
         <ResultList results={results}></ResultList>
     </div>;
 }
