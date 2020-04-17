8bd27be32572e46bb5b194b1aa52a08899fa8694
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-04-17 17:48:47 +0200

    Add some more sample data

--- put-test-data.sh
+++ put-test-data.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-Type: application/json" -d @./data/samples/find1.json localhost:9200/idai-field/resource/f1
-curl -XPOST -H "Content-Type: application/json" -d @./data/samples/find2.json localhost:9200/idai-field/resource/f2
+curl -XPOST -H "Content-Type: application/x-ndjson" --data-binary @data/samples/bulk.jsonl localhost:9200/idai-field/_bulk
