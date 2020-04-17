b651bd791f269b2fe1b48be90d6580c3fe8b4955
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-04-17 16:44:26 +0200

    Wrap SearchBar in Form

--- ui/src/SearchBar.tsx
+++ ui/src/SearchBar.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Icon from '@mdi/react';
 import { mdiMagnify } from '@mdi/js';
 
 
-type SearchBarProps = {
-    onSubmit: (query: string) => void
-}
-
-
-export default (props: SearchBarProps) => {
+export default (props: {onSubmit: (query: string) => void}) => {
 
     let query = '';
 
+    const submit = (event: FormEvent) => {
+        event.preventDefault();
+        props.onSubmit(query);
+    }
+
     return (
-        <InputGroup style={{zIndex: 1, position: 'absolute'}}>
-            <FormControl
-                placeholder="Suchen ..."
-                aria-label="Suchbegriff"
-                onChange={(event: any) => query = event.target.value}
-            />
-            <InputGroup.Append>
-                <Button variant="secondary" onClick={() => props.onSubmit(query)}>
-                    <Icon path={mdiMagnify} size={0.8}/>
-                </Button>
-            </InputGroup.Append>
-        </InputGroup>
+        <Form onSubmit={submit}>
+            <InputGroup style={{zIndex: 1, position: 'absolute'}}>
+                <FormControl
+                    placeholder="Suchen ..."
+                    aria-label="Suchbegriff"
+                    onChange={(event: any) => query = event.target.value}
+                />
+                <InputGroup.Append>
+                    <Button variant="secondary" type="submit">
+                        <Icon path={mdiMagnify} size={0.8}/>
+                    </Button>
+                </InputGroup.Append>
+            </InputGroup>
+        </Form>
     );
 
 }
