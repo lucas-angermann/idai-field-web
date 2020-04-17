c8a711c67a2da522e3d2866566bd94392547dab2
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-04-17 12:40:35 +0200

    Refactor atomize

--- api/lib/api/utils.ex
+++ api/lib/api/utils.ex
@@ -1,13 +1,11 @@
 defmodule Api.Utils do
 
-  def atomize(map) do
-    for {key, val} <- map, into: %{}, do: {String.to_atom(key),
-      case val do
-        [] -> []
-        list = [_|_] -> for item <- list, into: [], do: atomize item
-        map = %{} -> atomize map
-        value -> value
-      end
-    }
+  def atomize(%{} = map) do
+    for {key, val} <- map, into: %{}, do: { String.to_atom(key), atomize val }
   end
+
+  def atomize([_|_] = list), do: for item <- list, into: [], do: atomize item
+
+  def atomize(v), do: v
+
 end
