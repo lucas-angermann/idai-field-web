f08520c57ce32f7af4f60c94429ce434b83033e7
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-04-17 23:51:53 +0100

    Put test data to resource type within idai-field index

--- put-test-data.sh
+++ put-test-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-Type: application/x-ndjson" --data-binary @data/samples/bulk.jsonl localhost:9200/idai-field/_bulk
+curl -XPOST -H "Content-Type: application/x-ndjson" --data-binary @data/samples/bulk.jsonl localhost:9200/idai-field/resource/_bulk
