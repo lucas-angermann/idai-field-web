29ff31b132561596afa29dcaad4d1e3dea15a253
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-04-17 15:16:21 +0200

    Remove markers before rendering new result

--- ui/src/Map.tsx
+++ ui/src/Map.tsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect, CSSProperties } from 'react';
+import React, { useRef, useState, useEffect, useReducer, CSSProperties } from 'react';
 import mapboxgl, { LngLatLike } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViYXN0aWFuY3V5IiwiYSI6ImNrOTQxZjA4MzAxaGIzZnBwZzZ4c21idHIifQ._2-exYw4CZRjn9WoLx8i1A';
 
@@ -14,27 +15,30 @@ const mapContainerStyle: CSSProperties = {
 
 export default (props: { results: Array<any> }) => {
 
+    const [map, setMap] = useState(null);
+    const [, setMarkers] = useReducer(reduceMarkers, []);
     const mapOptions = { zoom: 2, center: [50, 50] };
     const mapContainer = useRef(null);
-    let map: mapboxgl.Map;
 
-    useEffect(() => {
-        map = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: mapOptions.center as LngLatLike,
-            zoom: mapOptions.zoom
-        });
-    }, []);
+    useEffect(() => { map ?? setMap(initializeMap(mapOptions, mapContainer.current)) }, [map, mapOptions])
 
-    useEffect(() => {
-        props.results.forEach(result => {
-            new mapboxgl.Marker()
-                .setLngLat(result.geometry.coordinates)
-                .addTo(map);
-        });
-    }, [props.results])
+    useEffect(() => setMarkers({ map, results: props.results }), [map, props.results]);
 
     return <div ref={mapContainer} style={mapContainerStyle}/>;
 
+}
+
+const initializeMap = (mapOptions, containerEl) => new mapboxgl.Map({
+    container: containerEl,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: mapOptions.center as LngLatLike,
+    zoom: mapOptions.zoom
+});
+
+const reduceMarkers = (markers, params) => {
+    markers.forEach(marker => marker.remove());
+    return params.results.map(result => new mapboxgl.Marker()
+        .setLngLat(result.geometry.coordinates)
+        .addTo(params.map)
+    );
 }
