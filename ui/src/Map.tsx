f1ad3cd45b5c5553ef5344cbda84a90af5903d93
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-04-23 13:58:12 +0200

    Show points & polygons on map

--- ui/src/Map.tsx
+++ ui/src/Map.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect, CSSProperties } from 'react';
 import mapboxgl, { Map, LngLatBounds, Layer, GeoJSONSourceRaw } from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { FeatureCollection } from 'geojson';
+import {Feature, FeatureCollection} from 'geojson';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViYXN0aWFuY3V5IiwiYSI6ImNrOTQxZjA4MzAxaGIzZnBwZzZ4c21idHIifQ._2-exYw4CZRjn9WoLx8i1A';
 
@@ -27,7 +27,7 @@ const polygonLayer: Layer = {
         'fill-opacity': 0.4
     },
     filter: ['==', '$type', 'Polygon']
-}
+};
 
 
 const pointLayer: Layer = {
@@ -39,8 +39,7 @@ const pointLayer: Layer = {
         'circle-color': '#B42222'
     },
     filter: ['==', '$type', 'Point']
-}
-
+};
 
 type MapOptions = { zoom: number, center: [number, number]};
 
@@ -48,28 +47,25 @@ type MapOptions = { zoom: number, center: [number, number]};
 export default ({ resources }: { resources: any[] }) => {
 
     const [map, setMap] = useState(null);
-    const mapOptions: MapOptions = { zoom: 2, center: [50, 50] };
+    const [ready, setReady] = useState(false);
+    const mapOptions: MapOptions = { zoom: 2, center: [0, 0] };
     const mapContainer = useRef(null);
 
     useEffect(() => {
-        if (!map) setMap(initializeMap(mapOptions, mapContainer.current));
+        if (!map) setMap(initializeMap(mapOptions, mapContainer.current, setReady));
     }, [map, mapOptions]);
 
     useEffect(() => {
         if (map && map.getSource(SOURCE_ID)) {
             map.getSource(SOURCE_ID).setData(getFeatureCollection(resources));
-        } else if (map) {
-            map.on('sourcedata', () => {
-                map.getSource(SOURCE_ID).setData(getFeatureCollection(resources));
-            });
         }
-    }, [map, resources]);
+    }, [map, resources, ready]);
 
     return <div ref={mapContainer} style={mapContainerStyle}/>;
 }
 
 
-const initializeMap = (mapOptions: MapOptions, containerEl: HTMLElement) => {
+const initializeMap = (mapOptions: MapOptions, containerEl: HTMLElement, setReady: Function) => {
 
     const map = new Map({
         container: containerEl,
@@ -79,16 +75,32 @@ const initializeMap = (mapOptions: MapOptions, containerEl: HTMLElement) => {
     });
 
     return map.on('load', () => {
-        map.addLayer(polygonLayer)
-            .addLayer(pointLayer)
-            .addSource(SOURCE_ID, { type: 'geojson', data: getFeatureCollection([]) });
+        map.addSource(SOURCE_ID, getGeoJSONSource())
+            .addLayer(polygonLayer)
+            .addLayer(pointLayer);
+        setReady(true);
     });
-}
+};
+
+
+const getGeoJSONSource = (): GeoJSONSourceRaw => ({
+    type: 'geojson',
+    data: getFeatureCollection([])
+});
 
 
 const getFeatureCollection = (resources: any[]): FeatureCollection => ({
     type: 'FeatureCollection',
-    features: resources.map(resource => resource.geometry)
+    features: resources.map(getFeature)
+});
+
+
+const getFeature = (resource: any): Feature => ({
+    type: 'Feature',
+    geometry: resource.geometry,
+    properties: {
+        resource: resource
+    }
 });
 
 
--- ui/src/search.ts
+++ ui/src/search.ts
@@ -1,5 +1,5 @@
 export const search = async (query: String): Promise<any> => {
 
-    const response = await fetch(`/resources/?q=${query}`);
+    const response = await fetch(`/resources/?q=${query}&size=2000`);
     return await response.json();
-}
+};
