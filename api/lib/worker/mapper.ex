b2043e2dff1dc73bece62036ef03587636aa470d
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-04-30 12:40:28 +0200

    Remove IO.inspect

--- api/lib/worker/mapper.ex
+++ api/lib/worker/mapper.ex
@@ -1,7 +1,6 @@
 defmodule Mapper do
 
     def process(change = %{ doc: %{ resource: %{ "type" => "Project" }}}) do
-        IO.inspect change
         id = change.doc.resource["identifier"]
         change = put_in(change.doc.resource["id"], id)
         put_in(change.id, id)
