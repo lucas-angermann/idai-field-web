f5659278ceb246f54a43b7528648d5b49d3197b6
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-05-05 10:54:50 +0200

    Remove unused ResultList component

--- ui/src/ResultList.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default (props: { results: any[] }) => <ul>{ props.results.map(renderResult) }</ul>;
-
-const renderResult = (result: any) => <li>{ result.identifier }</li>;
