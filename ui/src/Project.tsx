9aedf65562f47288506cc788f8cdbd901b46a57f
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-05-06 13:09:40 +0200

    Render aggregations in project component

--- ui/src/Project.tsx
+++ ui/src/Project.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { search, get } from './documents';
 import DocumentTeaser from './DocumentTeaser';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import DocumentList from './DocumentList';
 
 
@@ -13,6 +13,7 @@ export default () => {
 
     const { id } = useParams();
     const [documents, setDocuments] = useState([]);
+    const [aggregations, setAggregations] = useState([]);
     const [offset, setOffset] = useState(0);
     const [projectDocument, setProjectDocument] = useState(null);
 
@@ -21,13 +22,16 @@ export default () => {
         const el = e.currentTarget;
         if (el.scrollTop + el.clientHeight >= el.scrollHeight) {
             const newOffset = offset + CHUNK_SIZE;
-            getDocuments(id, newOffset).then(newDocs => setDocuments(documents.concat(newDocs)));
+            searchDocuments(id, newOffset).then(result => setDocuments(documents.concat(result.hits)));
             setOffset(newOffset);
         }
     };
 
     useEffect(() => {
-        getDocuments(id, 0).then(setDocuments);
+        searchDocuments(id, 0).then(result => {
+            setDocuments(result.hits);
+            setAggregations(result.aggregations);
+        });
         get(id).then(setProjectDocument);
     }, [id]);
 
@@ -36,6 +40,7 @@ export default () => {
             <Row>
                 <Col sm={ 3 }>
                     { renderProjectTeaser(projectDocument) }
+                    { renderAggregations(aggregations) }
                 </Col>
                 <Col onScroll={ onScroll } style={ { height: 'calc(100vh - 56px)', overflow: 'auto' } }>
                     <DocumentList documents={ documents } />
@@ -46,7 +51,7 @@ export default () => {
 
 };
 
-const getDocuments = async (id: string, from: number) => {
+const searchDocuments = async (id: string, from: number) => {
 
     const query = {
         q: `project:${id}`,
@@ -54,8 +59,27 @@ const getDocuments = async (id: string, from: number) => {
         from,
         skipTypes: ['Project', 'Image', 'Photo', 'Drawing']
     };
-    return (await search(query)).hits;
+    return search(query);
 };
 
 const renderProjectTeaser = (projectDocument: any) =>
     projectDocument ? <DocumentTeaser document={ projectDocument } /> : '';
+
+const renderAggregations = (aggregations: any) =>
+    Object.keys(aggregations).map((key: string) => renderAggregation(key, aggregations[key]));
+
+const renderAggregation = (key: string, aggregation: any) => (
+    <Card key={ key }>
+        <Card.Header>{ key }</Card.Header>
+        <Card.Body>
+            { aggregation.buckets.map(renderBucket) }
+        </Card.Body>
+    </Card>
+);
+
+const renderBucket = (bucket: any) => (
+    <Row key={ bucket.key }>
+        <Col>{ bucket.key }</Col>
+        <Col><em>{ bucket.doc_count }</em></Col>
+    </Row>
+);
