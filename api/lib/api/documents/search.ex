994777187fe32a97621b1fd4b16555fb63d22e30
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-05-06 12:15:26 +0200

    Generate type aggregation when searching

--- api/lib/api/documents/search.ex
+++ api/lib/api/documents/search.ex
@@ -5,17 +5,45 @@ defmodule Api.Documents.Search do
     q = if !q do "*" else q end
     size = if !size do 100 else size end
     from = if !from do 0 else from end
-    handle_result HTTPoison.get("#{get_base_url()}/_search", [], params: %{q: q, size: size, from: from})
+    handle_result HTTPoison.post(
+      "#{get_base_url()}/_search",
+      build_query(q, size, from),
+      [{"Content-Type", "application/json"}]
+    )
+  end
+
+  defp build_query(q, size, from) do
+    %{
+      query: %{
+        query_string: %{
+          query: q
+        },
+      },
+      size: size,
+      from: from,
+      aggs: build_aggregations(),
+      track_total_hits: true
+    } |> Poison.encode!
+  end
+
+  defp build_aggregations() do
+    %{
+      types: %{
+        terms: %{
+          field: "resource.type"
+        }
+      }
+    }
   end
 
   defp handle_result({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     body
     |> to_atomized_result
-    |> to_hits
-    |> Enum.map(&to_document/1)
+    |> update_in([:hits], fn(hits) -> Enum.map(hits.hits, &to_document/1) end)
   end
 
-  defp handle_result({:ok, %HTTPoison.Response{status_code: 400}}) do
+  defp handle_result({:ok, %HTTPoison.Response{status_code: 400, body: body}}) do
+    IO.inspect body
     %{error: "bad_request"}
   end
 
