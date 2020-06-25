20eef9668e7ca672f4fb16c4303d5ae79715970b
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-06-25 14:54:45 +0200

    Show error on project overview when backend is not available

--- ui/src/ProjectOverview.tsx
+++ ui/src/ProjectOverview.tsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Map from './Map';
 import { search } from './documents';
+import { Alert } from 'react-bootstrap';
 
 
 export default () => {
 
     const [projectDocuments, setProjectDocuments] = useState([]);
+    const [error, setError] = useState(false);
 
     useEffect (() => {
-        getProjectDocuments().then(setProjectDocuments);
+        getProjectDocuments()
+            .then(setProjectDocuments)
+            .catch(err => setError(err));
     }, []);
 
     return (
         <div>
-            <Map documents={ projectDocuments }></Map>
+            { error ? renderError(error) : renderMap(projectDocuments)}
         </div>
     );
 };
+
+
+const renderError = (error: any) => <Alert variant="danger">Backend not available!</Alert>;
+
+
+const renderMap = (projectDocuments: any) => <Map documents={ projectDocuments }></Map>;
 
 
 const getProjectDocuments = async () => (await search({ q: 'resource.type:Project' })).documents;
