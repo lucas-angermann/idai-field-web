657ae02279454844c8ed8cc31bf90810c425c9c2
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-01 10:40:42 +0200

    Handle deletes in indexer

--- api/lib/worker/indexer.ex
+++ api/lib/worker/indexer.ex
@@ -6,36 +6,40 @@ defmodule Indexer do
   defguard is_error(status_code) when status_code >= 400
 
   def process(change = %{deleted: true}) do
-    # TODO: Handle deletions
-    IO.puts "Deleted: #{change.id}"
+    # TODO: changes stream may include deletes for unindexed documents
+    handle_result HTTPoison.delete(get_doc_url(change.id))
   end
 
   def process(change) do
     handle_result HTTPoison.put(
-      "#{Config.get(:elasticsearch_url)}/#{Config.get(:elasticsearch_index)}/_doc/#{change.id}",
+      get_doc_url(change.id),
       Poison.encode!(change.doc),
       [{"content-type", "application/json"}]
     )
+  end
+
+  defp get_doc_url(id) do
+    "#{Config.get(:elasticsearch_url)}/#{Config.get(:elasticsearch_index)}/_doc/#{id}"
   end
 
   defp handle_result({:ok, %HTTPoison.Response{status_code: status_code, body: body}})
     when is_ok(status_code) do
 
     result = Poison.decode!(body)
-    IO.puts "Successfully indexed: #{result["_id"]}"
+    IO.puts "Successfully updated index: #{result["result"]} #{result["_id"]}"
   end
 
   defp handle_result({:ok, %HTTPoison.Response{status_code: status_code, body: body}})
     when is_error(status_code) do
 
     result = Poison.decode!(body)
-    IO.puts "ERROR: Indexing failed, result: #{inspect result}"
+    IO.puts "ERROR: Updating index failed, result: #{inspect result}"
     nil
   end
 
   defp handle_result({:error, %HTTPoison.Error{reason: reason}}) do
 
-    IO.puts "ERROR: Indexing failed, reason: #{inspect reason}"
+    IO.puts "ERROR: Updating index failed, reason: #{inspect reason}"
     nil
   end
 end
