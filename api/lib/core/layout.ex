4fb5e6d109e23b0ec2dd83657b6b54a06f7d9144
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-07-02 11:27:39 +0100

    Add placeholder groups to doc

--- api/lib/core/layout.ex
+++ api/lib/core/layout.ex
@@ -1,70 +1,22 @@
-"""
-{
-    "resource": {
-        "identifier": "F-1",
-        "shortDescription": "Schöner Fund",
-        "geometry": {},
-        "id": "134f-1235-f134g",
-        "type": "Find",
-        "groups": [
-            {
-                "name": "position",
-                "label": {
-                    "de": "Lage / Kontext",
-                    "en": "Position / context"
-                },
-                "relations": [
-                    {
-                        "name": "isAbove",
-                        "label": {
-                            "de": "Liegt über",
-                            "en": "Above"
-                        },
-                        "targets": [
-                            {
-                                "id": "134f-1235-f134g",
-                                "identifier": "F-2",
-                                "shortDescription": "Schöne Münze",
-                                "type": "Coin"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                "name": "dimesions",
-                "label": {
-                    "de": "Maße",
-                    "en": "Dimensions"
-                }
-                "fields": [
-                    {
-                        "name": "width",
-                        "label": {
-                            "de": "Breite",
-                            "en": "Width"
-                        },
-                        "value": "1cm"
-                    },
-                    {
-                        "name": "height",
-                        "label": {
-                            "de": "Höhe",
-                            "en": "Height"
-                        },
-                        "value": "2cm"
-                    }
-                ]
-            }
-        ]
-    }
-}
-"""
 
 defmodule Core.Layout do
 
-    def get_layout(doc, project_conf) do
-        # TODO
-    end
+  def get_layout(doc, %{ "groups" => groups }) do
 
+    doc = put_in(doc, [:resource, "groups"], [])
+
+    Enum.reduce(groups, doc,
+      fn group, doc ->
+
+        update_in(doc, [:resource, "groups"],
+          fn groups ->
+
+            # TODO see if we find fields in resource which are in that group
+            # if there are none return groups, otherwise attach a new group to groups
+            groups ++ [0]
+          end
+        )
+      end
+    )
+  end
 end
---
+++ api/test/core/layout_test.exs
@@ -0,0 +1,27 @@
+defmodule Core.LayoutTest do
+    use ExUnit.Case, async: true
+    use Plug.Test
+    alias Core.Layout
+
+  test "map object" do
+    doc = %{
+        resource: %{
+            "type" => "Operation",
+            "width" => "1cm",
+            "height" => "2cm",
+            "id" => "42"
+        }
+    }
+
+    %{ "de" => [%{ "item" => category_conf } | _] } = ProjectConfigLoader.load("test/resources", "test-project", ["de", "en"])
+    # TODO category = Treelist.get_by_predicate -> get_by_by_category_name
+
+    layouted_doc = Layout.get_layout(doc, category_conf)
+    #IO.inspect layouted_doc
+    assert get_in(layouted_doc.resource, ["id"]) == "42"
+    assert get_in(layouted_doc.resource, ["type"]) == "Operation"
+    assert get_in(layouted_doc.resource, ["width"]) == "1cm"
+    assert get_in(layouted_doc.resource, ["height"]) == "2cm"
+    assert length(get_in(layouted_doc.resource, ["groups"])) == 2
+  end
+end
