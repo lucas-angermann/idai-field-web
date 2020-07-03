2d1710ccea5eaf895384a687c4154858142d7a0d
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-07-03 15:29:32 +0200

    Return lists in scan functions

--- api/lib/core/layout.ex
+++ api/lib/core/layout.ex
@@ -29,40 +29,41 @@ defmodule Core.Layout do
         relations: scan_and_add(&scan_relation/2, config_group_relations, resource),
         name: config_group_name
     }
-    if group.fields != nil or group.relations != nil, do: group, else: nil
+    if group.fields != nil or group.relations != nil, do: [group], else: []
   end
 
   defp scan_relation(%{ "name" => config_relation_name}, resource) do
 
     if Map.has_key?(get_in(resource, ["relations"]), config_relation_name) do
-      %{
-        name: config_relation_name,
-        targets: get_in(resource, ["relations", config_relation_name])
-      }
+      [
+        %{
+          name: config_relation_name,
+          targets: get_in(resource, ["relations", config_relation_name])
+        }
+      ]
     else
-      nil
+      []
     end
   end
 
   defp scan_field(%{ "name" => config_field_name }, resource) do
 
     if Map.has_key?(resource, config_field_name) do
-      %{
-        name: config_field_name,
-        value: get_in(resource, [config_field_name])
-      }
+      [
+        %{
+          name: config_field_name,
+          value: get_in(resource, [config_field_name])
+        }
+      ]
     else
-      nil
+      []
     end
   end
 
   defp scan_and_add(scan, config_items, resource) do
     Enum.reduce(config_items, [],
       fn config_item, out_items ->
-        case apply(scan, [config_item, resource]) do
-          nil      -> out_items
-          out_item -> out_items ++ [out_item]
-        end
+        out_items ++ apply(scan, [config_item, resource])
       end
     )
   end
