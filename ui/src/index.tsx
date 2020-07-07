c1e777490b9b6315e555de150f0badd894ab8fc2
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-07 11:37:52 +0200

    Remove unused reference to service worker

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './buttons.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
