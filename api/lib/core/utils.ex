359a97f95619579e45e416c24bddb7fb61252ea4
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-07-07 14:13:51 +0100

    Add variants of atomize for inclusions and exlusions

--- api/lib/core/utils.ex
+++ api/lib/core/utils.ex
@@ -3,9 +3,35 @@ defmodule Core.Utils do
   def atomize(%{} = map) do
     for {key, val} <- map, into: %{}, do: { String.to_atom(key), atomize val }
   end
-
   def atomize([_|_] = list), do: for item <- list, into: [], do: atomize item
-
   def atomize(v), do: v
 
+  def atomize(%{} = map, only) do
+
+    for {key, val} <- map, into: %{} do
+      {
+          if Enum.member?(only, String.to_atom(key)) do String.to_atom(key) else key end,
+          atomize val
+      }
+    end
+  end
+
+  def atomize_excl(%{} = map, excl) do
+
+    for {key, val} <- map, into: %{} do
+
+      if Enum.member?(excl, String.to_atom(key)) do
+        {
+          key,
+          val
+        }
+      else
+        {
+          String.to_atom(key),
+          atomize_excl(val, excl)
+        }
+      end
+    end
+  end
+  def atomize_excl(v, _excl), do: v
 end
--- api/test/core/utils_test.exs
+++ api/test/core/utils_test.exs
@@ -4,4 +4,22 @@ defmodule Core.UtilsTest do
   test "abc" do
     assert Core.Utils.atomize(%{ "a" => "b" }) == %{ a: "b" }
   end
+
+  test "only atomize certain keys, let others untouched" do
+    result = Core.Utils.atomize(%{ "a" => "b", "c" => "d" }, [:a])
+    assert result == %{ :a => "b", "c" => "d" }
+    assert result.a == "b"
+  end
+
+  test "exclusions" do
+    result = Core.Utils.atomize_excl(%{ "a" => "b", "c" => "d" }, [:c])
+    assert result == %{ :a => "b", "c" => "d" }
+    assert result.a == "b"
+  end
+
+  test "exclusions - recursive case" do
+    result = Core.Utils.atomize_excl(%{ "a" => %{ "c" => "d", "b" => "m" }}, [:c])
+    assert result == %{ :a => %{ "c" => "d", :b => "m" } }
+    assert result.a.b == "m"
+  end
 end
