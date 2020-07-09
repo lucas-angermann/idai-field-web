d1989aeea17d6b37b2dc219e6228504a266bf75e
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-09 18:16:07 +0200

    Simplify backend search module

--- api/lib/api/documents/search.ex
+++ api/lib/api/documents/search.ex
@@ -7,34 +7,29 @@ defmodule Api.Documents.Search do
   @fields_geometries ["resource.type", "resource.geometry", "resource.identifier", "resource.id"]
 
   def by(q, size, from, filters, must_not, exists) do
-    query = Query.init(q, size, from)
-    |> Query.track_total()
+    Query.init(q, size, from)
+    |> Query.track_total
     |> Query.add_aggregations()
     |> Query.add_filters(filters)
     |> Query.add_must_not(must_not)
     |> Query.add_exists(exists)
-    |> Query.build()
-
-    handle_result HTTPoison.post(
-      "#{get_base_url()}/_search",
-      query,
-      [{"Content-Type", "application/json"}]
-    )
+    |> Query.build
+    |> post_query
   end
 
   def geometries_by(q, filters, must_not, exists) do
-    query = Query.init(q, @max_geometries)
+    Query.init(q, @max_geometries)
     |> Query.add_filters(filters)
     |> Query.add_must_not(must_not)
     |> Query.add_exists(exists)
     |> Query.only_fields(@fields_geometries)
-    |> Query.build()
+    |> Query.build
+    |> post_query
+  end
 
-    handle_result HTTPoison.post(
-      "#{get_base_url()}/_search",
-      query,
-      [{"Content-Type", "application/json"}]
-    )
+  defp post_query(query) do
+    HTTPoison.post("#{get_base_url()}/_search", query, [{"Content-Type", "application/json"}])
+    |> handle_result
   end
 
   defp handle_result({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
