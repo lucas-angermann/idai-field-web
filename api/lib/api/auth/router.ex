661213c1d82bb7c6d4f3cb41ad6f058e5b9354f8
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-10 15:23:54 +0200

    Remove elixir compiler warning

--- api/lib/api/auth/router.ex
+++ api/lib/api/auth/router.ex
@@ -6,10 +6,7 @@ defmodule Api.Auth.Router do
   plug :match
   plug :dispatch
 
-
-  @doc """
-  One may or may not pass a Bearer token when calling this
-  """
+  # One may or may not pass a Bearer token when calling this
   get "/show" do
     bearer = List.first get_req_header(conn, "authorization")
     rights = get_user_for_bearer(bearer)
@@ -22,25 +19,21 @@ defmodule Api.Auth.Router do
     |> send_json(%{ status: "ok", rights: rights })
   end
 
-
-  @doc """
-  As body, pass json like this
-  {
-     "name": "user-1",
-     "pass": "pass-1"
-  }
-
-  Issues a token for the user, which can be used in follow up requests
-  to claim to be that same user
-  """
+  # As body, pass json like this
+  # {
+  #    "name": "user-1",
+  #    "pass": "pass-1"
+  # }
+  #
+  # Issues a token for the user, which can be used in follow up requests
+  # to claim to be that same user
   post "/sign_in" do
-    {:ok, token, claims} = Auth.Guardian.encode_and_sign(conn.body_params)
+    {:ok, token, _claims} = Auth.Guardian.encode_and_sign(conn.body_params)
 
     conn
     |> put_resp_content_type("text/plain")
     |> send_json(%{ token: token })
   end
-
 
   defp get_user_for_bearer(nil), do: Auth.Rights.empty()
 
