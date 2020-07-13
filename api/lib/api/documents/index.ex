9641eacf15db3ba936cd6d3adca224aedf542157
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-13 13:55:02 +0200

    Use multi index search in documents endpoint

--- api/lib/api/documents/index.ex
+++ api/lib/api/documents/index.ex
@@ -39,7 +39,7 @@ defmodule Api.Documents.Index do
   end
 
   defp get_base_url do
-    "#{Application.fetch_env!(:api, :elasticsearch_url)}/#{Application.fetch_env!(:api, :elasticsearch_index)}"
+    "#{Application.fetch_env!(:api, :elasticsearch_url)}/#{Application.fetch_env!(:api, :elasticsearch_index)}_*"
   end
 
   defp post_query(query) do
