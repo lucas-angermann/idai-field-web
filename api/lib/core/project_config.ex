79fa958b89283fc7721aa8469747f36dd7d14d5e
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-14 17:46:30 +0200

    Add function for getting field label from config

---
+++ api/lib/core/project_config.ex
@@ -0,0 +1,12 @@
+defmodule Core.ProjectConfig do
+
+  def get_label(config, category, field) do
+    with category_config <- Core.CategoryTreeList.find_by_name(category, config),
+         fields_config <- Enum.flat_map(category_config.groups, &(&1.fields)),
+         field_config <- Enum.find(fields_config, &(&1.name == field))
+    do
+      field_config.label
+    end
+  end
+
+end
---
+++ api/test/unit/core/project_config_test.exs
@@ -0,0 +1,16 @@
+defmodule Core.ProjectConfigTest do
+  use ExUnit.Case, async: true
+  alias Core.ProjectConfig
+  alias Core.ProjectConfigLoader
+
+  test "get label" do
+    start_supervised({ProjectConfigLoader, {"test/resources", ["test-project"]}})
+    configuration = ProjectConfigLoader.get("test-project")
+
+    label = ProjectConfig.get_label(configuration, "Operation", "category")
+    assert label == %{ en: "Category", de: "Kategorie"}
+
+    label = ProjectConfig.get_label(configuration, "Operation", "height")
+    assert label == %{ en: "Height", de: "Höhe"}
+  end
+end
