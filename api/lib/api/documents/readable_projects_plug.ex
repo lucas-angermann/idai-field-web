19f7222cab38f0941375503cc0125b56079af971
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-07-23 00:45:00 +0100

    Add and use readable_projects plug

---
+++ api/lib/api/documents/readable_projects_plug.ex
@@ -0,0 +1,18 @@
+defmodule Api.Documents.ReadableProjectsPlug do
+  import Plug.Conn
+  
+  def init(options) do
+    options
+  end
+  
+  def call(conn, _opts) do
+    put_private(conn, :readable_projects, get_readable_projects(conn))
+  end
+
+  defp get_readable_projects conn do
+    (conn
+     |> get_req_header("authorization")
+     |> List.first
+     |> Api.Auth.Router.get_user_for_bearer).readable_projects
+  end
+end
--- api/lib/api/documents/router.ex
+++ api/lib/api/documents/router.ex
@@ -5,11 +5,10 @@ defmodule Api.Documents.Router do
   import Core.Layout
 
   plug :match
+  plug Api.Documents.ReadableProjectsPlug
   plug :dispatch
 
   get "/" do
-    readable_projects = get_readable_projects conn
-
     send_json(conn, Index.search(
       conn.params["q"] || "*",
       conn.params["size"] || 100,
@@ -17,26 +16,23 @@ defmodule Api.Documents.Router do
       conn.params["filters"],
       conn.params["not"],
       conn.params["exists"],
-      readable_projects || []
+      conn.private[:readable_projects]
     ))
   end
 
   get "/map" do
-    readable_projects = get_readable_projects conn
-    
     send_json(conn, Index.search_geometries(
       conn.params["q"] || "*",
       conn.params["filters"],
       conn.params["not"],
       conn.params["exists"],
-      readable_projects || []
+      conn.private[:readable_projects]
     ))
   end
 
   get "/:id" do
-
     with doc <- Index.get(id),
-         :ok <- access_for_project_allowed(conn, doc.project),
+         :ok <- access_for_project_allowed(conn.private[:readable_projects], doc.project),
          config <- Core.ProjectConfigLoader.get(doc.project),
          layouted_doc <- put_in(doc.resource, to_layouted_resource(config, doc.resource))
     do
@@ -47,8 +43,7 @@ defmodule Api.Documents.Router do
     end
   end
 
-  defp access_for_project_allowed conn, project do
-    readable_projects = get_readable_projects conn
+  defp access_for_project_allowed readable_projects, project do
     if project in readable_projects, do: :ok, else: :unauthorized_access
   end
 
