60e68d791d6bd6f75988f8427d085ea6af0f8df3
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-22 15:34:01 +0200

    Render logout link as Button

--- ui/src/Navbar.tsx
+++ ui/src/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties, useContext } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { useLocation, Link } from 'react-router-dom';
 import { LoginContext } from './App';
 import { LoginData } from './login';
@@ -23,7 +23,7 @@ export default ({ onLogout }: { onLogout: () => void }) => {
                     <Link to="/manual">Handbuch</Link>
                 </Nav.Link>
             </Nav>
-            <Navbar.Text className="mr-sm-2">{ renderLogin(loginData, onLogout) }</Navbar.Text>
+            { renderLogin(loginData, onLogout) }
         </Navbar>
     );
 };
@@ -31,8 +31,9 @@ export default ({ onLogout }: { onLogout: () => void }) => {
 
 const renderLogin = (loginData: LoginData, onLogout: () => void) =>
     loginData.user === 'anonymous'
-        ? <Link to="/login">Login</Link>
-        : <span>Eingeloggt als: { loginData.user } <a href="#" onClick={ onLogout }>Ausloggen</a></span>;
+        ? <Navbar.Text className="mr-sm-2"><Link to="/login">Login</Link></Navbar.Text>
+        : [<Navbar.Text>Eingeloggt als: { loginData.user }</Navbar.Text>,
+        <Button variant="link" onClick={ onLogout }>Ausloggen</Button>];
 
 
 const navbarStyle: CSSProperties = {
