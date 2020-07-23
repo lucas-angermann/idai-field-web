203f5d096dc44f3c48b63eed60469b94eae4c632
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-23 11:03:02 +0200

    Also show filters menu when doc is selected

--- ui/src/project/Project.tsx
+++ ui/src/project/Project.tsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, CSSProperties, useContext } from 'react';
-import { useParams, useLocation, useHistory } from 'react-router-dom';
+import { useParams, useLocation, useHistory, Link } from 'react-router-dom';
 import ProjectHome from './ProjectHome';
 import ProjectMap from './ProjectMap';
 import DocumentInfo from './DocumentInfo';
-import { get, mapSearch } from '../api/documents';
+import { get, mapSearch, search } from '../api/documents';
 import { Document } from '../api/document';
-import { Spinner } from 'react-bootstrap';
-import { ResultDocument, Result } from '../api/result';
+import { Spinner, Card, Row, Col } from 'react-bootstrap';
+import { ResultDocument, Result, FilterBucket, ResultFilter } from '../api/result';
 import { buildProjectQueryTemplate, addFilters } from '../api/query';
 import { History } from 'history';
 import { LoginContext } from '../App';
+import Icon from '@mdi/react';
+import { mdiCloseCircle } from '@mdi/js';
+import { NAVBAR_HEIGHT } from '../constants';
 
 
 const MAX_SIZE = 10000;
@@ -22,6 +25,7 @@ export default function Project() {
     const history = useHistory();
     const loginData = useContext(LoginContext);
     const [document, setDocument] = useState<Document>(null);
+    const [filters, setFilters] = useState<ResultFilter[]>([]);
     const [documents, setDocuments] = useState<ResultDocument[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
 
@@ -29,6 +33,11 @@ export default function Project() {
         documentId ? get(documentId, loginData.token)
             .then(setDocument) : setDocument(null);
     }, [documentId, loginData]);
+
+    useEffect(() => {
+        initFilters(projectId, location.search, loginData.token)
+            .then(result => setFilters(result.filters));
+    }, [projectId, location.search, loginData]);
 
     useEffect(() => {
         setLoading(true);
@@ -56,10 +65,21 @@ export default function Project() {
                     documents={ documents }
                     onDocumentClick={ onDocumentClick(history, location.search) }/>
             </div>
+            <div key="filters" style={ filtersContainerStyle }>
+                { renderFilters(filters, location.search) }
+            </div>
         </div>
     );
 
 }
+
+
+const initFilters = async (id: string, searchParams: string, token: string): Promise<Result> => {
+
+    const query = buildProjectQueryTemplate(id, 0, 0);
+    addFilters(query, searchParams);
+    return search(query, token);
+};
 
 
 const searchMapDocuments = async (id: string, searchParams: string, token: string): Promise<Result> => {
@@ -77,9 +97,74 @@ const onDocumentClick = (history: History, searchParams: string) => {
 };
 
 
+const renderFilters = (filters: ResultFilter[], searchParams: string) =>
+    filters.map((filter: ResultFilter) => renderFilter(filter, searchParams));
+
+
+const renderFilter = (filter: ResultFilter, searchParams: string) => (
+
+    <Card key={ filter.name }>
+        <Card.Header><h3>{ filter.label.de }</h3></Card.Header>
+        <Card.Body>
+            { filter.values.map((bucket: FilterBucket) => renderFilterValue(filter.name, bucket, searchParams)) }
+        </Card.Body>
+    </Card>
+);
+
+
+const renderFilterValue = (key: string, bucket: FilterBucket, searchParams: string) => {
+
+    return (
+        <Row key={ bucket.value }>
+            <Col>
+                <Link to={ addFilterToLocation(searchParams, key, bucket.value) }>
+                    { bucket.value }
+                </Link>
+                { renderCloseButton(searchParams, key, bucket.value) }
+            </Col>
+            <Col sm={ 3 } className="text-right"><em>{ bucket.count }</em></Col>
+        </Row>
+    );
+};
+
+
+const addFilterToLocation = (searchParams: string, key: string, value: string): string => {
+
+    const urlParams = new URLSearchParams(searchParams);
+    urlParams.append(key, value);
+    return `?${urlParams.toString()}`;
+};
+
+
+const renderCloseButton = (searchParams: string, key: string, value: string) => {
+
+    const urlParams = new URLSearchParams(searchParams);
+    if ( (urlParams.has(key) && urlParams.getAll(key).includes(value) ) ) {
+        const newParams = urlParams.getAll(key).filter(v => v !== value);
+        urlParams.delete(key);
+        newParams.forEach(v => urlParams.append(key, v));
+        return <Link to={ `?${urlParams.toString()}` }>
+            <Icon path={ mdiCloseCircle } size={ 0.8 }/>
+        </Link>;
+    }
+    return '';
+};
+
+
 const spinnerStyle: CSSProperties = {
     position: 'absolute',
     top: '50vh',
     left: '50vw',
     zIndex: 1
+};
+
+
+const filtersContainerStyle: CSSProperties = {
+    height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
+    width: '400px',
+    position: 'absolute',
+    top: NAVBAR_HEIGHT,
+    right: '10px',
+    overflow: 'auto',
+    zIndex: 1000
 };
--- ui/src/project/ProjectHome.tsx
+++ ui/src/project/ProjectHome.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect, CSSProperties, useContext } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { Location } from 'history';
 import { search, get } from '../api/documents';
 import DocumentTeaser from '../document/DocumentTeaser';
-import { Row, Col, Card, Alert } from 'react-bootstrap';
+import { Col, Card, Alert } from 'react-bootstrap';
 import DocumentList from './DocumentList';
-import { mdiCloseCircle } from '@mdi/js';
-import Icon from '@mdi/react';
 import { buildProjectQueryTemplate, addFilters } from '../api/query';
-import { ResultFilter, FilterBucket, Result, ResultDocument } from '../api/result';
+import { Result, ResultDocument } from '../api/result';
 import { NAVBAR_HEIGHT } from '../constants';
 import { Document } from '../api/document';
 import { LoginContext } from '../App';
@@ -21,7 +17,6 @@ export default function ProjectHome({ id, searchParams = '' }: { id: string, sea
 
     const loginData = useContext(LoginContext);
     const [documents, setDocuments] = useState<ResultDocument[]>([]);
-    const [filters, setFilters] = useState<ResultFilter[]>([]);
     const [offset, setOffset] = useState(0);
     const [projectDocument, setProjectDocument] = useState<Document>(null);
     const [error, setError] = useState(false);
@@ -42,14 +37,14 @@ export default function ProjectHome({ id, searchParams = '' }: { id: string, sea
 
         searchDocuments(id, searchParams, 0, loginData.token).then(result => {
             setDocuments(result.documents);
-            setFilters(result.filters);
         }).catch(err => setError(err));
 
         get(id, loginData.token).then(setProjectDocument);
     }, [id, searchParams, loginData]);
 
     const renderResult = () => {
-        return [
+        
+        return (
             <div style={ leftOverlayStyle } key="left-overlay">
                 { renderProjectTeaser(projectDocument) }
                 <Card onScroll={ onScroll } style={ listContainerStyle }>
@@ -57,11 +52,8 @@ export default function ProjectHome({ id, searchParams = '' }: { id: string, sea
                         <DocumentList documents={ documents } searchParams={ searchParams } />
                     </Card.Body>
                 </Card>
-            </div>,
-            <div key="filters" style={ filtersContainerStyle }>
-                { renderFilters(filters, searchParams) }
             </div>
-        ];
+        );
     };
 
     return (
@@ -82,60 +74,6 @@ const searchDocuments = async (id: string, searchParams: string, from: number, t
 
 const renderProjectTeaser = (projectDocument: Document) =>
     projectDocument ? <Card><Card.Body><DocumentTeaser document={ projectDocument } /></Card.Body></Card> : '';
-
-
-const renderFilters = (filters: ResultFilter[], searchParams: string) =>
-    filters.map((filter: ResultFilter) => renderFilter(filter, searchParams));
-
-
-const renderFilter = (filter: ResultFilter, searchParams: string) => (
-
-    <Card key={ filter.name }>
-        <Card.Header><h3>{ filter.label.de }</h3></Card.Header>
-        <Card.Body>
-            { filter.values.map((bucket: FilterBucket) => renderFilterValue(filter.name, bucket, searchParams)) }
-        </Card.Body>
-    </Card>
-);
-
-
-const renderFilterValue = (key: string, bucket: FilterBucket, searchParams: string) => {
-
-    return (
-        <Row key={ bucket.value }>
-            <Col>
-                <Link to={ addFilterToLocation(searchParams, key, bucket.value) }>
-                    { bucket.value }
-                </Link>
-                { renderCloseButton(searchParams, key, bucket.value) }
-            </Col>
-            <Col sm={ 3 } className="text-right"><em>{ bucket.count }</em></Col>
-        </Row>
-    );
-};
-
-
-const addFilterToLocation = (searchParams: string, key: string, value: string): string => {
-
-    const urlParams = new URLSearchParams(searchParams);
-    urlParams.append(key, value);
-    return `?${urlParams.toString()}`;
-};
-
-
-const renderCloseButton = (searchParams: string, key: string, value: string) => {
-
-    const urlParams = new URLSearchParams(searchParams);
-    if ( (urlParams.has(key) && urlParams.getAll(key).includes(value) ) ) {
-        const newParams = urlParams.getAll(key).filter(v => v !== value);
-        urlParams.delete(key);
-        newParams.forEach(v => urlParams.append(key, v));
-        return <Link to={ `?${urlParams.toString()}` }>
-            <Icon path={ mdiCloseCircle } size={ 0.8 }/>
-        </Link>;
-    }
-    return '';
-};
 
 
 const renderError = (error: any) => {
@@ -161,15 +99,4 @@ const listContainerStyle: CSSProperties = {
     marginTop: '0',
     overflow: 'auto',
     flexGrow: 1
-};
-
-
-const filtersContainerStyle: CSSProperties = {
-    height: `calc(100vh - ${NAVBAR_HEIGHT}px)`,
-    width: '400px',
-    position: 'absolute',
-    top: NAVBAR_HEIGHT,
-    right: '10px',
-    overflow: 'auto',
-    zIndex: 1000
 };
