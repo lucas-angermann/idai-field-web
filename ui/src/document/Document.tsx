d9b66889bbded323e4b7440aac367b01b732bb24
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-07-29 16:06:15 +0200

    Remove unused dependency

--- ui/src/document/Document.tsx
+++ ui/src/document/Document.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, ReactNode, ReactElement } from 'react';
+import React, { useState, useEffect, useContext, ReactElement } from 'react';
 import { useParams } from 'react-router-dom';
 import DocumentDetails from './DocumentDetails';
 import { Container } from 'react-bootstrap';
