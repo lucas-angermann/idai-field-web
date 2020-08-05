1e496b92d70f8bffc87653d8a84235011173488d
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-08-05 12:10:07 +0200

    Proper error handling for image endpoint

--- api/lib/api/images/router.ex
+++ api/lib/api/images/router.ex
@@ -8,17 +8,18 @@ defmodule Api.Images.Router do
   plug :dispatch
 
   get "/:id" do
-    with doc <- Index.get(id),
-         :ok <- access_for_project_allowed(conn.private[:readable_projects], doc.project),
-         {:ok, image_data} <- File.read("images/#{doc.project}/#{id}")
+    with %{ project: project } <- Index.get(id),
+         :ok <- access_for_project_allowed(conn.private[:readable_projects], project),
+         {:ok, image_data} <- File.read("images/#{project}/#{id}")
     do
       conn
       |> put_resp_content_type("image/jpeg")
       |> send_resp(200, image_data)
     else
+      nil -> send_not_found(conn)
       :unauthorized_access -> send_unauthorized(conn)
-      {:error, reason} -> IO.puts "Error when reading file: #{reason}"
-      _ -> IO.puts "other error"
+      {:error, reason} -> send_error(conn, "Error when reading file: #{reason |> :file.format_error}")
+      _ -> send_error(conn, "Unknown error")
     end
   end
 
