208b9f6bc42f5d3a9712b039f000ac6568139e40
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-08-07 10:27:06 +0200

    Wrap Image component in memo

--- ui/src/image/Image.tsx
+++ ui/src/image/Image.tsx
@@ -1,9 +1,9 @@
 import React, { ReactElement, CSSProperties } from 'react';
 
-export default function Image({ id, style, className }
+export default React.memo(function Image({ id, style, className }
         : { id: string, style?: CSSProperties, className?: string }): ReactElement {
 
     if (!id) return null;
 
     return <img src={ `/api/images/${id}` } style={ style } className={ className } alt={ id }/>;
-}
+});
