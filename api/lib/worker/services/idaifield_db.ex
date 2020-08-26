941f533e672d78342e5b3e868c2f36a687bcf811
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-08-26 15:07:07 +0200

    Add get_target_docs function

--- api/lib/worker/services/idaifield_db.ex
+++ api/lib/worker/services/idaifield_db.ex
@@ -3,6 +3,24 @@ defmodule Services.IdaiFieldDb do
   alias Services.ResultHandler
   require Logger
 
+  # todo if to be moved
+  def get_target_docs(documents, get) do
+    Enum.reduce(documents, %{},
+      fn %{ resource: resource }, target_documents ->
+        if resource.relations == nil do
+          target_documents
+        else
+          Enum.reduce(resource.relations, target_documents,
+            fn {relation_name, relation_target_ids}, target_documents ->
+              Enum.reduce(relation_target_ids, target_documents,
+                fn relation_target_id, target_documents ->
+                  put_in(target_documents, [relation_target_id], get.(relation_target_id))
+                end)
+            end)
+        end
+      end)
+  end
+
   def get_doc(db, id) do
     auth = [hackney: [basic_auth: {Core.Config.get(:couchdb_user), Core.Config.get(:couchdb_password)}]]
     HTTPoison.get("#{Core.Config.get(:couchdb_url)}/#{db}/#{id}", %{}, auth)
@@ -18,5 +36,4 @@ defmodule Services.IdaiFieldDb do
     |> CorePropertiesAtomizing.format_changes
     |> update_in([Access.all(), :doc], &(Map.drop(&1, [:_id, :_rev, :_attachments])))
   end
-
 end
