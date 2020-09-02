242441d9d015cc19e0af44c78efb694316b20a71
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-02 17:07:22 +0200

    Forward image to api caller

--- api/lib/api/images/router.ex
+++ api/lib/api/images/router.ex
@@ -11,12 +11,14 @@ defmodule Api.Images.Router do
 
   get "/" do
     result = Api.Images.CantaloupeAdapter.get()
-    IO.inspect result
+    # IO.inspect result
     case result do
-      {:ok, res} -> nil
+      {:ok, %{ body: image_data }} -> conn
+                                      |> put_resp_content_type("image/jpeg")
+                                      |> put_resp_header("cache-control", "max-age=86400, private, must-revalidate")
+                                      |> send_resp(200, image_data)
       {:error, _error} -> nil
     end
-    # todo forward body as image
   end
 
   get "/:id" do
