5c0648be511c7185629098a109ecd384335b045c
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-02 18:09:17 +0200

    Get rid of obsolete enpoint

--- api/lib/api/images/router.ex
+++ api/lib/api/images/router.ex
@@ -28,21 +28,9 @@ defmodule Api.Images.Router do
     end
   end
 
-  get "/:id" do
-    # todo remove endpoint
-#    with %{ project: project } <- Index.get(id),
-#         :ok <- access_for_project_allowed(conn.private[:readable_projects], project),
-#         {:ok, image_data} <- File.read("#{Core.Config.get(:images_dir)}/#{project}/#{id}")
-#    do
-#      conn
-#      |> put_resp_content_type("image/jpeg")
-#      |> put_resp_header("cache-control", "max-age=86400, private, must-revalidate")
-#      |> send_resp(200, image_data)
-#    else
-#      nil -> send_not_found(conn)
-#      :unauthorized_access -> send_unauthorized(conn)
-#      {:error, reason} -> send_error(conn, "Error when reading file: #{reason |> :file.format_error}")
-#      _ -> send_error(conn, "Unknown error")
-#    end
-  end
+  # todo review
+  #      nil -> send_not_found(conn)
+  #      :unauthorized_access -> send_unauthorized(conn)
+  #      {:error, reason} -> send_error(conn, "Error when reading file: #{reason |> :file.format_error}")
+  #      _ -> send_error(conn, "Unknown error")
 end
