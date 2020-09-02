caa664fbdb734f8bd2a1da74bea0c0f91d9f6027
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-02 17:14:44 +0200

    Use with instead of case

--- api/lib/api/images/router.ex
+++ api/lib/api/images/router.ex
@@ -10,14 +10,12 @@ defmodule Api.Images.Router do
   plug :dispatch
 
   get "/:project/:id" do
-    IO.puts "#{project}:#{id}"
-    result = Api.Images.CantaloupeAdapter.get(project, id)
-    # IO.inspect result
-    case result do
-      {:ok, %{ body: image_data }} -> conn
-                                      |> put_resp_content_type("image/jpeg")
-                                      |> put_resp_header("cache-control", "max-age=86400, private, must-revalidate")
-                                      |> send_resp(200, image_data)
+    with {:ok, %{body: image_data}} <- Api.Images.CantaloupeAdapter.get(project, id) do
+      conn
+      |> put_resp_content_type("image/jpeg")
+      |> put_resp_header("cache-control", "max-age=86400, private, must-revalidate")
+      |> send_resp(200, image_data)
+    else
       {:error, _error} -> nil
     end
   end
