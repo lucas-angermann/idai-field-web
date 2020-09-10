9f6ecfb8db62853030cddff6d1890a545b8f8249
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-09-10 11:40:24 +0200

    Experiment with openlayers

--- ui/src/categoryColors.ts
+++ ui/src/categoryColors.ts
@@ -4,6 +4,17 @@ export const getColor = (categoryName: string): string => {
 };
 
 
+export const hexToRgb = (hex: string): { r: number, g: number, b: number } => {
+    
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
+
+
 /* tslint:disable:no-bitwise */
 export const isColorTooBright = (color: string): boolean => {
 
--- ui/src/project/ProjectMap.tsx
+++ ui/src/project/ProjectMap.tsx
@@ -1,15 +1,27 @@
-import React, { CSSProperties, ReactElement } from 'react';
-import { Map, GeoJSON, ZoomControl } from 'react-leaflet';
+import React, { CSSProperties, ReactElement, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { TFunction } from 'i18next';
-import L, { PathOptions } from 'leaflet';
-import { Feature, FeatureCollection } from 'geojson';
+import { Feature, FeatureCollection, Geometry } from 'geojson';
 import extent from 'turf-extent';
 import { NAVBAR_HEIGHT, SIDEBAR_WIDTH } from '../constants';
-import hash from 'object-hash';
-import { getColor } from '../categoryColors';
+import { getColor, hexToRgb } from '../categoryColors';
 import { ResultDocument } from '../api/result';
 import { Document } from '../api/document';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { Vector as VectorSource, TileImage } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { Circle as CircleStyle, Fill, Stroke, Style }  from 'ol/style';
+import TileGrid from 'ol/tilegrid/TileGrid';
+import { Feature as OlFeature } from 'ol';
+import GeoJSON from 'ol/format/GeoJSON';
+import { get as getProjection } from 'ol/proj';
+import proj4 from 'proj4';
+import { register } from 'ol/proj/proj4';
+
+
+proj4.defs('EPSG:32638', '+proj=utm +zone=38 +ellps=WGS84 +datum=WGS84 +units=m +no_defs');
+register(proj4);
 
 
 export default React.memo(function ProjectMap({ document, documents, onDocumentClick }
@@ -18,19 +30,12 @@ export default React.memo(function ProjectMap({ document, documents, onDocumentC
     const featureCollection = createFeatureCollection(documents, document);
     const { t } = useTranslation();
 
+    useEffect(() => {
+        createMap(featureCollection, onDocumentClick);
+    });
+
     return <>
-        <Map style={ mapStyle }
-            crs={ L.CRS.Simple }
-            minZoom={ -20 }
-            maxZoom={ 10 }
-            bounds={ getBounds(featureCollection, document) }
-            boundsOptions={ { paddingTopLeft: [410, 10], paddingBottomRight: [10, 10] } }
-            renderer={ L.canvas({ padding: 0.5 }) }
-            attributionControl={ false }
-            zoomControl={ false }>
-            { getGeoJSONElement(featureCollection, onDocumentClick) }
-            <ZoomControl position="bottomright" />
-        </Map>
+        <div id="ol-map" style={ mapStyle }/>
         { (!documents || documents.length === 0) && renderEmptyResult(t) }
     </>;
 }, (prevProps: any, nextProps: any) => {
@@ -40,47 +45,93 @@ export default React.memo(function ProjectMap({ document, documents, onDocumentC
 });
 
 
+const createMap = (featureCollection: FeatureCollection, onDocumentClick: (_: any) => void) => {
+
+    let layers = [];
+
+    layers = layers.concat(getTileLayer());
+    layers = layers.concat(getGeoJSONLayer(featureCollection, onDocumentClick));
+
+    return new Map({
+        target: 'ol-map',
+        layers,
+        view: new View({
+            projection: getProjection('EPSG:32638'),
+            center: [563249.985350, 3466750.014550],
+            resolution: 2.012063
+        })
+    });
+};
+
+
 const renderEmptyResult = (t: TFunction): ReactElement => {
 
     return <div style={ emptyResultStyle }>{ t('projectMap.noResources') }</div>;
 };
 
 
-const getGeoJSONElement = (featureCollection: FeatureCollection, onDocumentClick: (_: any) => void): ReactElement => {
+const getGeoJSONLayer = (featureCollection: FeatureCollection, onDocumentClick: (_: any) => void): VectorLayer[] => {
 
-    if (!featureCollection) return;
+    if (!featureCollection) return [];
 
-    return (
-        <GeoJSON key={ hash(featureCollection) }
-                 data={ featureCollection }
-                 pointToLayer={ pointToLayer }
-                 style={ getStyle }
-                 onEachFeature={ onEachFeature(onDocumentClick) } />
-    );
+    const vectorSource = new VectorSource({
+        features: new GeoJSON().readFeatures(featureCollection),
+    });
+
+    const vectorLayer = new VectorLayer({
+        source: vectorSource,
+        style: getStyle,
+    });
+
+    return [ vectorLayer ];
 };
 
 
-const pointToLayer = (feature: Feature, latLng: L.LatLng): L.CircleMarker => {
+const getTileLayer = (): TileLayer[] => {
 
-    return L.circleMarker(
-        latLng,
-        {
-            fillColor: getColor(feature.properties.category),
-            fillOpacity: 1,
+    const url = '/0127b1ce-696b-591d-a77a-3f22fa54432a_32638/{z}/{x}/{y}.png';
+
+    return [ new TileLayer({
+        source: new TileImage({
+            tileGrid: new TileGrid({
+                extent: [ 562998.477499999804, 3466498.50669999979, 563501.493200000143, 3467001.52240000013 ],
+                origin: [ 562998.477499999804, 3467001.52240000013 ],
+                resolutions: [ 2.0120628000014551, 1.00603140000072755, 0.503015700000363775, 0.251507850000181887,
+                    0.125753925000090944, 0.0628769625000454718],
+                tileSize: [256, 256]
+            }),
+            tileUrlFunction: (tileCoord) =>
+                url
+                    .replace('{z}', String(tileCoord[0]))
+                    .replace('{x}', String(tileCoord[1]))
+                    .replace('{y}', String(tileCoord[2]))
+            ,
+        })
+    }) ];
+};
+
+
+const getStyle = (feature: OlFeature): Style => {
+
+    const fill = new Fill({ color: getColorForCategory(feature.getProperties().category, 0.2) });
+    const stroke = new Stroke({ color: getColorForCategory(feature.getProperties().category, 1) });
+
+    return new Style({
+        image: new CircleStyle({
             radius: 5,
-            stroke: false,
-            pane: 'markerPane'
-        }
-    );
+            fill,
+            stroke,
+        }),
+        stroke,
+        fill
+    });
 };
 
 
-const getStyle = (feature: Feature): PathOptions => ({
-    color: getColor(feature.properties.category),
-    weight: feature.geometry.type === 'LineString' ? 2 : 1,
-    opacity: 0.5,
-    fillOpacity: feature.geometry.type === 'Polygon' || feature.geometry.type === 'MultiPolygon' ? 0.2 : 1
-});
+const getColorForCategory = (category: string, opacity: number): string => {
+    const rgb = hexToRgb(getColor(category));
+    return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${opacity})`;
+};
 
 
 const onEachFeature = (onDocumentClick: (_: any) => void) => (feature: Feature, layer: L.Layer): void => {
@@ -116,7 +167,7 @@ const onClick = (onDocumentClick: (_: any) => void) => (event: any): void => {
 };
 
 
-const createFeatureCollection = (documents: any[], selectedDocument: any): FeatureCollection | undefined => {
+const createFeatureCollection = (documents: any[], selectedDocument: any): any => {
 
     if (documents.length === 0) return undefined;
 
@@ -124,7 +175,13 @@ const createFeatureCollection = (documents: any[], selectedDocument: any): Featu
         type: 'FeatureCollection',
         features: documents
             .filter(document => document?.resource.geometry)
-            .map(document => createFeature(document, document.resource.id === selectedDocument?.resource.id))
+            .map(document => createFeature(document, document.resource.id === selectedDocument?.resource.id)),
+        crs: {
+            'type': 'name',
+            'properties': {
+                'name': 'EPSG:32638'
+            }
+        }
     };
 };
 
@@ -169,4 +226,83 @@ const emptyResultStyle: CSSProperties = {
     left: '50vw',
     transform: `translate(calc(-50% + ${SIDEBAR_WIDTH / 2}px), -50%)`,
     zIndex: 1
+};
+
+const geoJSONStyles = {
+    'Point': new Style({
+        image: new CircleStyle({
+            radius: 5,
+            fill: null,
+            stroke: new Stroke({
+                color: 'red',
+                width: 1
+            }),
+        }),
+    }),
+    'LineString': new Style({
+        stroke: new Stroke({
+            color: 'green',
+            width: 1,
+        }),
+    }),
+    'MultiLineString': new Style({
+        stroke: new Stroke({
+            color: 'green',
+            width: 1,
+        }),
+    }),
+    'MultiPoint': new Style({
+        image: new CircleStyle({
+            radius: 5,
+            fill: null,
+            stroke: new Stroke({
+                color: 'red',
+                width: 1
+            }),
+        }),
+    }),
+    'MultiPolygon': new Style({
+        stroke: new Stroke({
+            color: 'yellow',
+            width: 1,
+        }),
+        fill: new Fill({
+            color: 'rgba(255, 255, 0, 0.1)',
+        }),
+    }),
+    'Polygon': new Style({
+        stroke: new Stroke({
+            color: 'blue',
+            lineDash: [4],
+            width: 3,
+        }),
+        fill: new Fill({
+            color: 'rgba(0, 0, 255, 0.1)',
+        }),
+    }),
+    'GeometryCollection': new Style({
+        stroke: new Stroke({
+            color: 'magenta',
+            width: 2,
+        }),
+        fill: new Fill({
+            color: 'magenta',
+        }),
+        image: new CircleStyle({
+            radius: 10,
+            fill: null,
+            stroke: new Stroke({
+                color: 'magenta',
+            }),
+        }),
+    }),
+    'Circle': new Style({
+        stroke: new Stroke({
+            color: 'red',
+            width: 2,
+        }),
+        fill: new Fill({
+            color: 'rgba(255,0,0,0.2)',
+        }),
+    }),
 };
