1db89d602ca5357717bcbf10307ccbbc447bb80e
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-25 22:47:03 +0100

    Rescale image before cropping

--- api/lib/worker/services/tiles_calculator.ex
+++ api/lib/worker/services/tiles_calculator.ex
@@ -5,11 +5,30 @@ defmodule Worker.Services.TilesCalculator do
     size = 6000
     tile_size = 256
 
-    commands = Enum.flat_map(create_template(size, tile_size), fn {entries, z} ->
+    template = create_template(size, tile_size)
+
+    commands = Enum.map(template, fn {{rescale, entries}, z} ->
+
+      rescale = floor(rescale)
+      IO.puts "rescale to #{rescale}"
+      System.cmd("convert",
+             [
+               "/imageroot/wes/6c5f936b-dba9-bf57-b681-5fc292e00e0b.jpg",
+               "-resize",
+               "#{rescale}x#{rescale}",
+               "/imageroot/wes/6c5f936b-dba9-bf57-b681-5fc292e00e0b.#{rescale}.jpg"
+             ]
+      )
       Enum.map(entries,
+
         fn entry ->
 
-          make_commands(tile_size, "6c5f936b-dba9-bf57-b681-5fc292e00e0b.jpg","6c5f936b-dba9-bf57-b681-5fc292e00e0b",z, entry)
+           {cmd_a, args_a, cmd_b, args_b} = make_commands(tile_size, "6c5f936b-dba9-bf57-b681-5fc292e00e0b.#{rescale}.jpg","6c5f936b-dba9-bf57-b681-5fc292e00e0b",z, entry)
+           System.cmd(cmd_a, args_a)
+           IO.puts "#{cmd_b} #{inspect args_b}"
+           System.cmd(cmd_b, args_b)
+
+           entry
 
         end)
     end)
@@ -23,16 +42,22 @@ defmodule Worker.Services.TilesCalculator do
          input,
          output,
          z_index,
-         %{x_index: x_index, y_index: y_index, x_pos: x_pos, y_pos: y_pos, tile_size: tile_size}) do
+         %{x_index: x_index, y_index: y_index, x_pos: x_pos, y_pos: y_pos}) do
 
     {
-    "mkdir -p /imageroot/wes/#{output}/#{z_index}/#{x_index}/#{y_index}",
-    "convert"
-    <> " /imageroot/wes/#{input}"
-    <> " -crop #{tile_size}x#{tile_size}+#{x_pos}+#{y_pos}"
-    <> " -background transparent"
-    <> " -extent #{output_tile_size}x#{output_tile_size}"
-    <> " /imageroot/wes/#{output}/#{z_index}/#{x_index}/#{y_index}.png"}
+      "mkdir",
+      [
+        "-p", "/imageroot/wes/#{output}/#{z_index}/#{x_index}"
+      ],
+      "convert",
+      [
+        "/imageroot/wes/#{input}",
+        "-crop", "256x256+#{x_pos}+#{y_pos}",
+        "-background", "transparent",
+        "-extent", "#{output_tile_size}x#{output_tile_size}",
+        "/imageroot/wes/#{output}/#{z_index}/#{x_index}/#{y_index}.png"
+      ]
+    }
   end
 
   defp create_template(size, output_tile_size) do
@@ -41,7 +66,8 @@ defmodule Worker.Services.TilesCalculator do
       if run != :ok do
         nil
       else
-        template = List.flatten(calc_template(current_size, output_tile_size, scale_factor))
+        tmp = calc_template(current_size, output_tile_size, scale_factor)
+        template = {current_size, List.flatten(tmp)}
         if current_size < output_tile_size do
           {template, {:halt, current_size, scale_factor*2}}
         else
@@ -67,9 +93,8 @@ defmodule Worker.Services.TilesCalculator do
                 %{
                   x_index: x_val,
                   y_index: y_val,
-                  tile_size: scale_factor * tile_size,
-                  x_pos: x_val * tile_size * scale_factor,
-                  y_pos: y_val * tile_size * scale_factor
+                  x_pos: x_val * tile_size,
+                  y_pos: y_val * tile_size
                 }
               ]
             end
