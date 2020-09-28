faefcdaa1c00a3588b1a950f8272daa799c4d801
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-28 20:24:28 +0100

    Search in elasticsearch for georeferenced images for tile generation

--- api/lib/worker/services/tiles_calculator.ex
+++ api/lib/worker/services/tiles_calculator.ex
@@ -30,9 +30,16 @@ defmodule Worker.Services.TilesCalculator do
     and required_delegates_present(delegates)
   end
 
-#    size = 6000
-#    project = "wes"
-#    image_id = "6c5f936b-dba9-bf57-b681-5fc292e00e0b"
+  # size = 6000
+  # project = "wes"
+  # image_id = "6c5f936b-dba9-bf57-b681-5fc292e00e0b"
+  # todo it is necessary for indexing to run before tile generation
+  def trigger_tile_calculation() do
+    %{ documents: docs } = Api.Documents.Index.search "*", 10, 0, [], [], ["resource.georeference"], ["wes"]
+    entries = Enum.map(docs, fn %{resource: %{ :id => id, "width" => width, "height" => height }} -> {id, width, height} end)
+    IO.inspect entries
+    nil
+  end
 
   def calc_tiles(project, image_id, image_size = {size_x, size_y}) do
 
