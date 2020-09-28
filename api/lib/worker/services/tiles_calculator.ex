fed3f66e34499c7889351fcd448aec3300af3e6b
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-28 19:22:49 +0100

    Write test to check imagemagick environment

--- api/lib/worker/services/tiles_calculator.ex
+++ api/lib/worker/services/tiles_calculator.ex
@@ -2,6 +2,33 @@ defmodule Worker.Services.TilesCalculator do
 
   @imageroot "/imageroot/"
   @tile_size 256
+  @required_imagemagick_version [6, 9]
+  @required_delegates ["jp2", "png", "jpeg"]
+
+  def required_version_matches(version, [required_major, required_minor]) do
+    [major, minor, _] = String.split(version, ".")
+    {major, _} = Integer.parse major
+    {minor, _} = Integer.parse minor
+    major > required_major or (major == required_major and minor >= required_minor)
+  end
+
+  def required_delegates_present(delegates) do
+    Enum.empty?(@required_delegates -- delegates)
+  end
+
+  def environment_ready() do
+    {result, result_status} = System.cmd("convert", ["--version"])
+    lines = String.split(result, "\n")
+    [delegates] = Enum.filter(lines, fn line -> String.starts_with?(line, "Delegates") end)
+    [_, delegates] = String.split(delegates, ":")
+    delegates = String.split(delegates)
+    [version] = Enum.filter(lines, fn line -> String.starts_with?(line, "Version") end)
+    [version] = Enum.filter(String.split(version), fn part -> String.match?(part, ~r/\d*\.\d\.\d/) end)
+
+    result_status == 0
+    and required_version_matches(version, @required_imagemagick_version)
+    and required_delegates_present(delegates)
+  end
 
 #    size = 6000
 #    project = "wes"
