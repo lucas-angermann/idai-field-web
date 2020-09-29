005579544b5d29d9c29f7b2fb79957471fcda7ac
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-09-29 14:54:59 +0100

    Make tiling work for rectangles; improve code

--- api/lib/worker/services/tiles_calculator.ex
+++ api/lib/worker/services/tiles_calculator.ex
@@ -4,6 +4,7 @@ defmodule Worker.Services.TilesCalculator do
   @tile_size 256
   @required_imagemagick_version [6, 9]
   @required_delegates ["jp2", "png", "jpeg"]
+  @intermediate_format_suffix "jpg"
 
   def required_version_matches(version, [required_major, required_minor]) do
     [major, minor, _] = String.split(version, ".")
@@ -41,43 +42,38 @@ defmodule Worker.Services.TilesCalculator do
     nil
   end
 
-  def calc_tiles(project, image_id, image_size = {size_x, size_y}) do
-
-    template = create_template(image_size, @tile_size)
-
-    # todo: build all commands, then flatten, then execute them
+  def calc_tiles(project, image_id, image_size = {width, height}) do
+    template = create_template(Enum.max([width, height]), @tile_size)
     commands = Enum.map(template, fn {{rescale, entries}, z} ->
       rescale = floor(rescale)
-      {cmd, args} = make_rescale_command(project, rescale, rescale)
-      System.cmd(cmd, args)
+      {mkdir_cmd, mkdir_args, rescale_cmd, rescale_args} = make_rescale_commands(project, image_id, rescale)
+      System.cmd(mkdir_cmd, mkdir_args)
+      System.cmd(rescale_cmd, rescale_args)
       Enum.map(entries,
         fn entry ->
-           {cmd_a, args_a, cmd_b, args_b} = make_crop_commands(
-             project, @tile_size, "#{image_id}.#{rescale}.jpg", image_id, z, entry)
-           System.cmd(cmd_a, args_a)
-           System.cmd(cmd_b, args_b)
+           {mkdir_cmd, mkdir_args, crop_cmd, crop_args} = make_crop_commands(
+             project, @tile_size, image_id, rescale, z, entry)
+           System.cmd(mkdir_cmd, mkdir_args)
+           System.cmd(crop_cmd, crop_args)
            nil
         end)
     end)
     nil
   end
 
-  defp create_template(image_size = {size_x, size_y}, tile_size) do
-    Stream.unfold({:continue, image_size}, fn {run, current_size = {current_size_x, current_size_y}} ->
-      if run != :continue do
-        nil
-      else
+  defp create_template(image_size, tile_size) do
+    Stream.unfold({:continue, image_size}, fn {run, current_size} ->
+      if run != :continue, do: nil, else:
         {
           {
             current_size,
             List.flatten(calc_template(current_size, tile_size))
           },
           {
-            if current_size_x < tile_size do :halt else :continue end,
-            {current_size_x / 2, current_size_y / 2}
+            if current_size < tile_size do :halt else :continue end,
+            current_size / 2
           }
         }
-      end
     end) |> Enum.to_list() |> Enum.reverse() |> Enum.with_index()
   end
 
@@ -107,13 +103,18 @@ defmodule Worker.Services.TilesCalculator do
     )
   end
 
-  defp make_rescale_command(project, rescale_x, rescale_y) do
-    {"convert",
+  defp make_rescale_commands(project, image_id, rescale) do
+    {
+      "mkdir",
       [
-        @imageroot <> project <> "/6c5f936b-dba9-bf57-b681-5fc292e00e0b.jpg",
+        "-p", "#{@imageroot}#{project}/#{image_id}"
+      ],
+      "convert",
+      [
+        "#{@imageroot}#{project}/#{image_id}.jpg", # todo make param for suffix
         "-resize",
-        "#{rescale_x}x#{rescale_y}",
-        @imageroot <> project <> "/6c5f936b-dba9-bf57-b681-5fc292e00e0b.#{rescale_x}.jpg" # todo rescale_y
+        "#{rescale}x#{rescale}",
+        "#{@imageroot}#{project}/#{image_id}/#{image_id}.#{rescale}.#{@intermediate_format_suffix}"
       ]
     }
   end
@@ -121,22 +122,23 @@ defmodule Worker.Services.TilesCalculator do
   defp make_crop_commands(
          project,
          tile_size,
-         input,
-         output,
+         image_id,
+         rescale,
          z_index,
          %{x_index: x_index, y_index: y_index, x_pos: x_pos, y_pos: y_pos}) do
     {
       "mkdir",
       [
-        "-p", @imageroot <> project <> "/#{output}/#{z_index}/#{x_index}"
+        "-p", "#{@imageroot}#{project}/#{image_id}/#{z_index}/#{x_index}"
       ],
       "convert",
       [
-        @imageroot <> project <> "/#{input}",
+        "#{@imageroot}#{project}/#{image_id}/#{image_id}.#{rescale}.#{@intermediate_format_suffix}",
+        "-quiet", # suppress 'convert: geometry does not contain image', some tiles simply will have no content with tiled rectangular images
         "-crop", "#{tile_size}x#{tile_size}+#{x_pos}+#{y_pos}",
         "-background", "transparent",
         "-extent", "#{tile_size}x#{tile_size}",
-        "/imageroot/wes/#{output}/#{z_index}/#{x_index}/#{y_index}.png"
+        "/imageroot/wes/#{image_id}/#{z_index}/#{x_index}/#{y_index}.png"
       ]
     }
   end
