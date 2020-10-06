8911c8694e06eaa3bf3e7660e0dfd0c0f1c87910
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-10-06 11:43:32 +0200

    Improve CategoryFilter layout

--- ui/src/project/CategoryFilter.tsx
+++ ui/src/project/CategoryFilter.tsx
@@ -1,7 +1,7 @@
 import { mdiCloseCircle } from '@mdi/js';
 import Icon from '@mdi/react';
 import React, { CSSProperties, ReactElement, ReactNode } from 'react';
-import { Dropdown, ButtonGroup } from 'react-bootstrap';
+import { Dropdown, ButtonGroup, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { deleteFilterFromParams, addFilterToParams } from '../api/query';
 import { FilterBucket, FilterBucketTreeNode, ResultFilter } from '../api/result';
@@ -54,10 +54,14 @@ const renderFilterValue = (key: string, bucket: FilterBucketTreeNode, params: UR
                 key={ bucket.item.value.name }
                 style={ filterValueStyle(level) }
                 to={ '?' + addFilterToParams(params, key, bucket.item.value.name) }>
-            <CategoryIcon category={ bucket.item.value.name } size="30" />
-            { getLabel(bucket.item.value.name, bucket.item.value.label) }
-            { renderCloseButton(params, key, bucket.item.value.name) }
-            <span className="float-right"><em>{ bucket.item.count }</em></span>
+            <Row>
+                <Col xs={ 1 }><CategoryIcon category={ bucket.item.value.name } size="30" /></Col>
+                <Col style={ categoryLabelStyle }>
+                    { getLabel(bucket.item.value.name, bucket.item.value.label) }
+                    { renderCloseButton(params, key, bucket.item.value.name) }
+                </Col>
+                <Col xs={ 1 }><span className="float-right"><em>{ bucket.item.count }</em></span></Col>
+            </Row>
         </Dropdown.Item>
         { bucket.trees
             && bucket.trees.map((bucket: FilterBucketTreeNode) => renderFilterValue(key, bucket, params, level + 1)) }
@@ -76,5 +80,9 @@ const renderCloseButton = (params: URLSearchParams, key: string, value: string):
 
 const filterValueStyle = (level: number): CSSProperties => ({
     width: '350px',
-    paddingLeft: `${level * 1.5}em`
+    paddingLeft: `${level * 1.2}em`
 });
+
+const categoryLabelStyle: CSSProperties = {
+    margin: '3px 10px'
+};
