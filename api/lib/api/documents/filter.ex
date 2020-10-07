91b6aa90a57607291a0920a8fc3a181257293c5f
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-10-07 12:48:45 +0200

    Implement filter expansion

--- api/lib/api/documents/filter.ex
+++ api/lib/api/documents/filter.ex
@@ -7,8 +7,15 @@ defmodule Api.Documents.Filter do
 
   defp parse_filter_string(filter_string), do: String.split(filter_string, ":") |> List.to_tuple
 
-  defp expand_filter({"resource.category.name", value}, _) do
-    [{"resource.category.name", value}]
+  defp expand_filter({"resource.category.name", parent_category}, project_conf) do
+    [{"resource.category.name", parent_category}] ++
+      with %{trees: child_categories_conf} <- Enum.find(project_conf, &(&1.item.name == parent_category)),
+          child_categories <- Enum.map(child_categories_conf, &(&1.item.name))
+      do
+        Enum.map(child_categories, &({"resource.category.name", &1}))
+      else
+        _ -> []
+      end
   end
   defp expand_filter({field, value}, _), do: [{field, value}]
 end
