5d821280c2552f82b47e0e58c9ae53dffbae7f45
Sebastian Cuy <sebastian.cuy@dainst.de>
2020-10-07 12:26:27 +0200

    Refactor filter module

--- api/lib/api/documents/filter.ex
+++ api/lib/api/documents/filter.ex
@@ -3,12 +3,12 @@ defmodule Api.Documents.Filter do
   def parse(nil), do: nil
   def parse(filter_strings = [_|_]), do: Enum.map(filter_strings, &parse_filter_string/1)
 
-  def expand(filters = [_|_]), do: Enum.flat_map(filters, &expand_filter/1)
+  def expand(filters = [_|_], project_conf), do: Enum.flat_map(filters, &(expand_filter(&1, project_conf)))
 
   defp parse_filter_string(filter_string), do: String.split(filter_string, ":") |> List.to_tuple
 
-  defp expand_filter({"resource.category.name", value}) do
+  defp expand_filter({"resource.category.name", value}, _) do
     [{"resource.category.name", value}]
   end
-  defp expand_filter({field, value}), do: [{field, value}]
+  defp expand_filter({field, value}, _), do: [{field, value}]
 end
