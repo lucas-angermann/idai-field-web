f305073c80d825b975feae59e3c1eccdd522b8c2
Thomas Kleinke <thomas.kleinke@dainst.de>
2020-11-06 17:28:49 +0100

    Set all projects as readable for non-anonymous admin users

--- api/lib/api/auth/bearer.ex
+++ api/lib/api/auth/bearer.ex
@@ -5,29 +5,28 @@ defmodule Api.Auth.Bearer do
 
   @anonymous "anonymous"
 
-  def get_user_for_bearer(nil), do: anonymous_user()
+  def get_user_for_bearer(nil) do
+    anonymous_user()
+    |> update_readable_projects
+  end
   def get_user_for_bearer(bearer) do
     token = String.replace bearer, "Bearer ", ""
     case Guardian.resource_from_token(token) do
       {:ok, user, _} -> user
       _ -> anonymous_user() # todo write test
     end
+    |> update_readable_projects
   end
 
   defp anonymous_user do
     users = Core.Config.get(Api.Auth, :users)
     anonymous = (Enum.find(
                   users,
-                  %{name: @anonymous},
+                  %{ name: @anonymous },
                   fn user -> user.name == @anonymous end
                 ))
     is_admin = anonymous[:admin] == true
-
-    readable_projects = if is_admin do
-      Config.get(:projects)
-    else
-      Config.get(Api.Auth, :readable_projects)[@anonymous] || []
-    end
+    readable_projects = Config.get(Api.Auth, :readable_projects)[@anonymous] || []
 
     %{
       user: @anonymous,
@@ -35,5 +34,10 @@ defmodule Api.Auth.Bearer do
       readable_projects: readable_projects
     }
   end
+
+  defp update_readable_projects(user = %{ is_admin: true }) do
+    put_in(user.readable_projects, Config.get(:projects))
+  end
+  defp update_readable_projects(user), do: user
 end
 
