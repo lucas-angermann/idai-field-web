ab8e52fbcdcec2c522ce304a35bf6b908bf675e1
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-12-23 20:16:30 +0100

    Autoconvert relation names; refactor

--- api/lib/core/core_properties_atomizing.ex
+++ api/lib/core/core_properties_atomizing.ex
@@ -5,49 +5,22 @@ defmodule Core.CorePropertiesAtomizing do
   @core_properties [:groups, :relations, :shortDescription, :id, :type, :category, :identifier, :geometry, :gazId,
     :georeference, :parentId, :grandparentId]
 
-  # TODO make sure we dont forget any, since there is an interplay with [String.to_atom(config_item.name)] in scan_relation in layout.ex; review this interplay
-  @relations [
-    :liesWithin,
-    :isRecordedIn,
-    :isChildOf,
-    :isAfter,
-    :isBefore,
-    :isContemporaryWith,
-    :isAbove,
-    :isBelow,
-    :isEquivalentTo,
-    :isInstanceof,
-    :hasInstance,
-    :borders,
-    :cuts,
-    :isCutBy,
-    :depicts,
-    :isDepictedIn,
-    :isSameAs
-  ]
-
   def get_core_properties(), do: @core_properties
 
+  @doc """
+  Takes a document and changes map keys from strings to atoms
+  where
+    it is on the document level itself
+    it is a resource core property
+    it is a relation name
+
+  supports nested documents, when relation targets are documents themselves
+  """
   def format_document(document) do
     document = document
     |> atomize([:resource])
     |> atomize([:resource] ++ @core_properties, true)
-
-    if Map.has_key?(document.resource, :relations) do
-      document
-      |> update_in([:resource, :relations], fn rel -> atomize(rel, @relations, true) end)
-      |> update_in([:resource, :relations],
-        fn rel -> for {k, v} <- rel, into: %{} do
-            if is_list(v) do
-              { k, Enum.map(v, fn item -> format_document(item) end) }
-            else
-              { k, v }
-            end
-          end
-        end)
-    else
-      document
-    end
+    |> update_relations
   end
 
   def format_changes(changes) do
@@ -55,5 +28,19 @@ defmodule Core.CorePropertiesAtomizing do
     |> atomize([:doc])
     |> atomize([:doc], true)
     |> Enum.map(&(update_in(&1, [:doc], fn doc -> format_document(doc) end)))
+  end
+
+  defp update_relations(document) do
+    if not Map.has_key?(document.resource, :relations), do: document, else:
+      document
+      |> update_in([:resource, :relations],
+           fn rel -> for {k, v} <- rel, into: %{} do
+                       {
+                         (if is_atom(k), do: k, else: String.to_atom(k)),
+                         (if is_list(v), do: Enum.map(v, &format_document/1), else: v)
+                       }
+                     end
+           end
+         )
   end
 end
--- api/test/unit/core/utils_test.exs
+++ api/test/unit/core/utils_test.exs
@@ -1,77 +1,76 @@
 defmodule Core.UtilsTest do
   use ExUnit.Case
-
+  import Core.Utils
+  
   test "base case" do
-    assert Core.Utils.atomize(%{ "a" => "b" }) == %{ a: "b" }
+    assert atomize(%{ "a" => "b" }) == %{ a: "b" }
   end
 
-  test "2 args" do
-    assert Core.Utils.atomize(%{ "a" => "b" }, [:a]) == %{ "a" => "b" }
-  end
-
-  test "only atomize certain keys, let others untouched" do
-    result = Core.Utils.atomize(%{ "a" => "b", "c" => "d" }, [:a], true)
-    assert result == %{ :a => "b", "c" => "d" }
-    assert result.a == "b"
+  test "base case - is recursive by default" do
+    assert atomize(%{ "a" => %{ "d" => "e" }}) == %{ a: %{ d: "e" } }
   end
 
   test "exclusions" do
-    result = Core.Utils.atomize(%{ "a" => "b", "c" => "d" }, [:c])
+    result = atomize(%{ "a" => "b", "c" => "d" }, [:c])
+    assert result == %{ :a => "b", "c" => "d" }
+  end
+
+  test "exclusions - recursive case" do
+    result = atomize(%{ "a" => %{ "c" => "d", "b" => "m" }}, [:c])
+    assert result == %{ :a => %{ "c" => "d", :b => "m" } }
+  end
+
+  test "inclusions" do
+    result = atomize(%{ "a" => "b", "c" => "d" }, [:a], true)
     assert result == %{ :a => "b", "c" => "d" }
     assert result.a == "b"
   end
 
-  test "exclusions - recursive case" do
-    result = Core.Utils.atomize(%{ "a" => %{ "c" => "d", "b" => "m" }}, [:c])
-    assert result == %{ :a => %{ "c" => "d", :b => "m" } }
-    assert result.a.b == "m"
-  end
-
   test "pass list" do
-    result = Core.Utils.atomize([%{ "a" => "d", "b" => "m" }])
+    result = atomize([%{ "a" => "d", "b" => "m" }])
     assert result == [%{ :a => "d", :b => "m" }]
   end
 
   test "pass list, explicit exclusion" do
-    result = Core.Utils.atomize([%{ "a" => "d", "b" => "m" }], [:b])
+    result = atomize([%{ "a" => "d", "b" => "m" }], [:b])
     assert result == [%{ :a => "d", "b" => "m" }]
   end
 
   test "exclusions - recursive case, with lists" do
-    result = Core.Utils.atomize(%{ "a" => [%{ "a" => "d", "b" => "m" }]}, [:b])
+    result = atomize(%{ "a" => [%{ "a" => "d", "b" => "m" }]}, [:b])
     assert result == %{ :a => [%{ :a => "d", "b" => "m" }] }
   end
 
   test "map contains already atomized key" do
-    result = Core.Utils.atomize(%{ a: "b"}, [:a], true)
+    result = atomize(%{ a: "b"}, [:a], true)
     assert result == %{ a: "b" }
   end
 
   test "up to" do
-    result = Core.Utils.atomize_up_to(%{ "a" => %{ "b" => "c" }}, :a)
+    result = atomize_up_to(%{ "a" => %{ "b" => "c" }}, :a)
     assert result == %{ a: %{ "b" => "c" } }
 
-    result = Core.Utils.atomize_up_to(%{ "a" => %{ "b" => %{ "c" => "d" } }}, :b)
+    result = atomize_up_to(%{ "a" => %{ "b" => %{ "c" => "d" } }}, :b)
     assert result == %{ a: %{ b: %{ "c" => "d" }} }
 
-    result = Core.Utils.atomize_up_to(%{ "a" => 3 }, :a)
+    result = atomize_up_to(%{ "a" => 3 }, :a)
     assert result == %{ a: 3 }
 
-    result = Core.Utils.atomize_up_to(%{ "a" => 3 }, :b)
+    result = atomize_up_to(%{ "a" => 3 }, :b)
     assert result == %{ a: 3 }
   end
 
   test "up to - with arrays" do
-    result = Core.Utils.atomize_up_to([%{ "a" => %{ "b" => "c" }}], :a)
+    result = atomize_up_to([%{ "a" => %{ "b" => "c" }}], :a)
     assert result == [%{ a: %{ "b" => "c" } }]
 
-    result = Core.Utils.atomize_up_to(%{ "a" => [%{ "b" => %{ "c" => "d" } }]}, :b)
+    result = atomize_up_to(%{ "a" => [%{ "b" => %{ "c" => "d" } }]}, :b)
     assert result == %{ a: [%{ b: %{ "c" => "d" }}] }
 
-    result = Core.Utils.atomize_up_to(%{ "a" => [3] }, :a)
+    result = atomize_up_to(%{ "a" => [3] }, :a)
     assert result == %{ a: [3] }
 
-    result = Core.Utils.atomize_up_to(%{ "a" => [3] }, :b)
+    result = atomize_up_to(%{ "a" => [3] }, :b)
     assert result == %{ a: [3] }
   end
 end
