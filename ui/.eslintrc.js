70b58a9ed771cb674ca08db21ac97bc920a384ed
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-01-07 17:23:08 +0100

    Replace TSLint with TypeScript ESLint

---
+++ ui/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    plugins: [
+        "eslint-plugin-prefer-arrow"
+    ],
+    rules: {
+        "@typescript-eslint/array-type": [
+            "error",
+            {
+                "default": "array"
+            }
+        ],
+        "@typescript-eslint/semi": [ "warn", "always" ],
+        "prefer-arrow/prefer-arrow-functions": [
+            "error",
+            {
+                "allowStandaloneDeclarations": true
+            }
+        ],
+        "max-len": [
+            "error",
+            {
+                "code": 120
+            }
+        ],
+        "no-trailing-spaces": [
+            "error",
+            {
+                "skipBlankLines": true
+            }
+        ],
+        "no-multiple-empty-lines": [
+            "error",
+            {
+                "max": 2
+            }
+        ],
+        "quotes": [ "error", "single" ],
+        "jsx-quotes": [ "error", "prefer-double" ]
+    }
+};
