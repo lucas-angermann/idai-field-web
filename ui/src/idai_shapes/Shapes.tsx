31557d6a2bd8aa0911136cc4144c48ba35edb0fa
Michael <michael.kihm@web.de>
2021-01-07 16:02:03 +0100

    User React Router Match hook for routing

--- ui/src/idai_shapes/Shapes.tsx
+++ ui/src/idai_shapes/Shapes.tsx
@@ -6,22 +6,27 @@ import { getPersistedLogin } from '../login';
 import { doLogout } from '../logout';
 import ShapesNav from '../shared/navbar/ShapesNav';
 import BrowseSelect from './browseselect/BrowseSelect';
+import { useRouteMatch } from 'react-router-dom';
 
-export default function Shapes({ match }: { match?: any }): ReactElement {
+export default function Shapes(): ReactElement {
     
     const [loginData, setLoginData] = useState(getPersistedLogin());
+    const match = useRouteMatch();
+
 
     useEffect(() => {
 
         document.title = 'iDAI.shapes';
     }, []);
 
+    const baseUrl = match.path || '/';
+
     return (
         <BrowserRouter>
             <LoginContext.Provider value={ loginData }>
                 <ShapesNav onLogout={ doLogout(setLoginData) } />
                 <Switch>
-                    <Route path=":documentId?" component={ BrowseSelect } />
+                    <Route path={ `${baseUrl}:documentId?` } component={ BrowseSelect } />
                 </Switch>
             </LoginContext.Provider>
         </BrowserRouter>
