73e71a0e00daeefe56d250ccfbe3048177280444
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-01-11 14:13:25 +0100

    Format eslinrc

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,52 +1,32 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2020,
-        sourceType: "module",
+        sourceType: 'module',
         ecmaFeatures: {
             jsx: true
         }
     },
     settings: {
         react: {
-            version: "detect"
+            version: 'detect'
         }
     },
     extends: [
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:@typescript-eslint/recommended",
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
     rules: {
-        "@typescript-eslint/array-type": [
-            "warn",
-            {
-                "default": "array"
-            }
-        ],
-        "@typescript-eslint/semi": [ "warn", "always" ],
-        "@typescript-eslint/no-inferrable-types": 0,
-        "max-len": [
-            "warn",
-            {
-                "code": 120
-            }
-        ],
-        "no-trailing-spaces": [
-            "warn",
-            {
-                "skipBlankLines": true
-            }
-        ],
-        "no-multiple-empty-lines": [
-            "warn",
-            {
-                "max": 2
-            }
-        ],
-        "quotes": [ "warn", "single" ],
-        "jsx-quotes": [ "warn", "prefer-double" ],
-        "object-curly-spacing": ["warn", "always"],
-        "no-multi-spaces": ["warn"]
+        '@typescript-eslint/array-type': [ 'warn', { 'default': 'array' } ],
+        '@typescript-eslint/semi': [ 'warn', 'always' ],
+        '@typescript-eslint/no-inferrable-types': 0,
+        'max-len': [ 'warn', { 'code': 120 } ],
+        'no-trailing-spaces': [ 'warn', { 'skipBlankLines': true } ],
+        'no-multiple-empty-lines': [ 'warn', { 'max': 2 } ],
+        'quotes': [ 'warn', 'single' ],
+        'jsx-quotes': [ 'warn', 'prefer-double' ],
+        'object-curly-spacing': ['warn', 'always'],
+        'no-multi-spaces': ['warn']
     }
 };
