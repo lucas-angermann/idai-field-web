3627aa0f468896c2d26a3485a21eef5039eee3b1
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-01-26 11:11:13 +0100

    Fix backend tests

--- api/lib/api/documents/mock_index_adapter.ex
+++ api/lib/api/documents/mock_index_adapter.ex
@@ -24,10 +24,10 @@ defmodule Api.Documents.MockIndexAdapter do
     decoded_and_atomized = Core.Utils.atomize(Poison.decode!(query))
 
 
-    hits = case decoded_and_atomized.query.bool.must.query_string.query do
+    hits = case decoded_and_atomized.query.script_score.query.bool.must.query_string.query do
       "*" ->
 
-        filters = decoded_and_atomized.query.bool.filter
+        filters = decoded_and_atomized.query.script_score.query.bool.filter
         %{ terms: %{ project: readable_projects }} = Enum.find(filters, fn filter -> Map.has_key? filter, :terms end)
         Enum.filter([@doc_a, @doc_b], fn doc -> doc.project in readable_projects end)
 
