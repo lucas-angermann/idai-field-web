5f4c40e3545a1c513818b452d277a5fabe31f921
Michael <michael.kihm@web.de>
2021-01-26 09:18:59 +0100

    load model from local server

--- ui/src/idai_shapes/draw/Draw.tsx
+++ ui/src/idai_shapes/draw/Draw.tsx
@@ -1,15 +1,40 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import { Row, Col, Button } from 'react-bootstrap';
+import * as tf from '@tensorflow/tfjs';
 
 export default function Draw(): ReactElement {
 
+    const [model, setModel] = useState<tf.LayersModel>();
+
     let saveableCanvas;
     const [brushRadius, setBrushRadius] = useState<number>(10);
+    const modelUrl = 'http://localhost:3000/week1/model/model.json';
+
+    const loadModel = async (url: string) => {
+    
+        const model = await tf.loadLayersModel(url);
+        await setModel(model);
+    };
+
+    useEffect(() => {
+
+        tf.ready().then(() => loadModel(modelUrl));
+    }, []);
 
     const findHandler = () => {
 
-        localStorage.setItem('savedDrawing', saveableCanvas.getSaveData());
+        if(model) {
+            const canvas = saveableCanvas.canvasContainer.childNodes[1];
+            const raw = tf.browser.fromPixels(canvas,3);
+            const resized = tf.image.resizeBilinear(raw, [512,512]);
+            const tensor = resized.expandDims(0);
+            const prediction = model.predict(tensor) as tf.Tensor;
+            alert(prediction.reshape([-1]));
+        }
+
+
+        //localStorage.setItem('savedDrawing', saveableCanvas.getSaveData());
     };
 
     const brushRadiusHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
