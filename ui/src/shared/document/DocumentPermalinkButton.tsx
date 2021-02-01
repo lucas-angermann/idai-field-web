9be052eb160426ee6a68d20e0bf454dabeb6969c
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-02-01 18:38:20 +0100

    Add translation

--- ui/src/shared/document/DocumentPermalinkButton.tsx
+++ ui/src/shared/document/DocumentPermalinkButton.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from 'react';
 import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
 import Icon from '@mdi/react';
 import { mdiLinkVariant } from '@mdi/js';
 import { Document } from '../../api/document';
@@ -8,7 +10,9 @@ import { Document } from '../../api/document';
 export default function DocumentLinkButton({ document, baseUrl }
         : { document: Document, baseUrl: string }): ReactElement {
 
-    return <OverlayTrigger trigger="click" placement="bottom" overlay={ getPopover(document, baseUrl) }>
+    const { t } = useTranslation();
+
+    return <OverlayTrigger trigger="click" placement="bottom" overlay={ getPopover(document, baseUrl, t) }>
         <Button variant="primary">
             <Icon path={ mdiLinkVariant } size={ 0.8 } />
         </Button>
@@ -16,9 +20,9 @@ export default function DocumentLinkButton({ document, baseUrl }
 }
 
 
-const getPopover = (document: Document, baseUrl: string): ReactElement =>
+const getPopover = (document: Document, baseUrl: string, t: TFunction): ReactElement =>
     <Popover id="document-link-popover" style={ { width: '350px', maxWidth: '350px' } }>
-      <Popover.Title as="h3">Permalink der Ressource</Popover.Title>
+      <Popover.Title as="h3">{ t('permalinkButton.title') } </Popover.Title>
       <Popover.Content>
         <input disabled value={ `${baseUrl}/document/${document.resource.id}` } style={ { width: '100%' } }></input>
       </Popover.Content>
