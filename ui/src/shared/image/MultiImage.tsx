4175c7342f33cea7480d99e7401f1cf09b2bb23c
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-02-01 11:49:37 +0100

    Adjust layout of MultiImage

--- ui/src/shared/image/MultiImage.tsx
+++ ui/src/shared/image/MultiImage.tsx
@@ -57,5 +57,6 @@ export default React.memo(function MultiImage({ project, id, maxWidth, maxHeight
 const multiImageStyle: CSSProperties = {
     position: 'relative',
     width: '100%',
-    height: '100%'
+    height: '100%',
+    overflow: 'hidden'
 };
