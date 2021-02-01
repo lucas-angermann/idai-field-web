9d8cfa5be2b5e216b9d3027f299eb0b6cd75e713
Sebastian Cuy <sebastian.cuy@dainst.de>
2021-02-01 07:35:17 +0100

    Simplify DocumentHierarchy

--- ui/src/shared/documents/DocumentHierarchy.tsx
+++ ui/src/shared/documents/DocumentHierarchy.tsx
@@ -1,15 +1,94 @@
-import React, { ReactElement } from 'react';
+import { mdiMenuLeft } from '@mdi/js';
+import Icon from '@mdi/react';
+import React, { CSSProperties, ReactElement, useRef } from 'react';
+import { Card } from 'react-bootstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { ResultDocument } from '../../api/result';
-import './document-hierarchy.css';
-import DocumentHierarchyBody from './DocumentHierarchyBody';
+import { getPreviousHierarchyLevelUrl } from '../../idai_field/project/navigation';
+import DocumentTeaser from '../document/DocumentTeaser';
+import LinkButton from '../linkbutton/LinkButton';
 
 
-export default function DocumentHierarchy(
-        { documents, searchParams }
-        : { documents: ResultDocument[], searchParams: string }): ReactElement {
-
-    return documents && (documents.length > 0
-        ? <DocumentHierarchyBody documents={ documents }
-            searchParams={ searchParams } />
-        : <></>);
+interface DocumentHierarchyProps {
+    documents: ResultDocument[];
+    searchParams?: string;
 }
+
+
+export default React.memo(function DocumentHierarchy({ documents, searchParams }
+        : DocumentHierarchyProps): ReactElement {
+
+    const parent = new URLSearchParams(searchParams).get('parent') ?? 'root';
+    const prevGrandparent = useRef<string>();
+
+    const backward = parent === prevGrandparent.current;
+    prevGrandparent.current = getGrandparent(documents);
+
+    const className = backward ? 'document-list-transition backward' : 'document-list-transition';
+
+
+    return <Card.Body className="px-0 py-0">
+        <TransitionGroup className={ className } style={ groupStyle } >
+            <CSSTransition key={ parent } timeout={ 500 }>
+                <div className="document-hierarchy">
+                    {
+                        parent !== 'root' &&
+                        <LinkButton
+                            to={ getPreviousHierarchyLevelUrl(getProjectId(documents), documents) }
+                            style={ previousHierarchyLevelButtonStyle } variant={ 'link' }>
+                            <Icon path={ mdiMenuLeft } size={ 1 } />
+                        </LinkButton>
+                    }
+                    { parent === 'root' && <div style={ previousHierarchyLevelButtonStyle } /> }
+                    <div className="documents" style={ documentsStyle }>
+                        { documents.map((document: ResultDocument) => {
+                            const linkUrl = `/project/${document.project}/${document.resource.id}${searchParams}`;
+                            return <div style={ documentContainerStyle } key={ document.resource.id }>
+                                <DocumentTeaser document={ document } searchParams={ searchParams } linkUrl={ linkUrl }
+                                                showHierarchyButton={ true } />
+                            </div>;
+                        }) }
+                    </div>
+                </div>
+            </CSSTransition>
+        </TransitionGroup>
+    </Card.Body>;
+}, (prevProps: DocumentHierarchyProps, nextProps: DocumentHierarchyProps) => {
+
+    return prevProps.documents === nextProps.documents;
+});
+
+
+const getGrandparent = (documents: ResultDocument[]): string => {
+
+    const grandparent = documents.length > 0 ? documents[0].resource.grandparentId : null;
+    return grandparent ?? 'root';
+};
+
+
+const getProjectId = (documents: ResultDocument[]): string => {
+
+    return documents.length > 0 ? documents[0].project : null;
+};
+
+
+const documentContainerStyle: CSSProperties = {
+    borderBottom: '1px solid var(--main-bg-color)'
+};
+
+
+const groupStyle: CSSProperties = {
+    height: '100%',
+    position: 'relative'
+};
+
+
+const previousHierarchyLevelButtonStyle: CSSProperties = {
+    height: '100%',
+    padding: 0
+};
+
+
+const documentsStyle: CSSProperties = {
+    flexGrow: 1
+};
