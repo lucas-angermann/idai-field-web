e6f637c20888dc87866db5455fd3e331f8c64571
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-02-02 16:03:26 +0100

    Autoselect permalink when opening popover

--- ui/src/shared/document/DocumentPermalinkButton.tsx
+++ ui/src/shared/document/DocumentPermalinkButton.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ReactElement } from 'react';
+import React, { CSSProperties, MutableRefObject, ReactElement, useRef } from 'react';
 import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { TFunction } from 'i18next';
@@ -10,10 +10,13 @@ import { Document } from '../../api/document';
 export default function DocumentLinkButton({ document, baseUrl }
         : { document: Document, baseUrl: string }): ReactElement {
 
+    const inputElementRef: MutableRefObject<HTMLInputElement> = useRef();
     const { t } = useTranslation();
 
-    return <OverlayTrigger trigger="click" placement="bottom" overlay={ getPopover(document, baseUrl, t) }>
-        <Button variant="secondary" style={ buttonStyle }>
+    return <OverlayTrigger trigger="click" placement="bottom"
+                           overlay={ getPopover(document, baseUrl, inputElementRef, t) }>
+        <Button variant="secondary" style={ buttonStyle }
+                onClick={ () => selectPermalink(inputElementRef) }>
             <div style={ iconStyle }>
               <Icon path={ mdiLinkVariant } size={ 0.7 } />
             </div>
@@ -22,13 +25,31 @@ export default function DocumentLinkButton({ document, baseUrl }
 }
 
 
-const getPopover = (document: Document, baseUrl: string, t: TFunction): ReactElement =>
+const getPopover = (document: Document, baseUrl: string, inputElementRef: MutableRefObject<HTMLInputElement>,
+                    t: TFunction): ReactElement =>
     <Popover id="document-link-popover" style={ popoverStyle }>
       <Popover.Title as="h3">{ t('permalinkButton.title') } </Popover.Title>
       <Popover.Content>
-        <input disabled value={ `${baseUrl}/document/${document.resource.id}` } style={ inputStyle }></input>
+        <input ref={ inputElementRef } readOnly
+          value={ `${baseUrl}/document/${document.resource.id}` }
+          style={ inputStyle } />
       </Popover.Content>
     </Popover>;
+
+
+const selectPermalink = (inputElementRef: MutableRefObject<HTMLInputElement>) => {
+
+  if (inputElementRef?.current) return;
+  
+  const observer = new MutationObserver(() => {
+    if (inputElementRef?.current) {
+      inputElementRef.current.select();
+      observer.disconnect();
+    }
+  });
+
+  observer.observe(document, { childList: true, subtree: true });
+};
 
 
 const buttonStyle: CSSProperties = {
