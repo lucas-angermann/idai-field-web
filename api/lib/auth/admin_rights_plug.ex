00cc63a985ff626e2717301eb99fadbf9de3683d
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-02-23 23:20:55 +0100

    Now simplify admin_rights_plug

--- api/lib/auth/admin_rights_plug.ex
+++ api/lib/auth/admin_rights_plug.ex
@@ -1,24 +1,16 @@
 defmodule Api.Auth.AdminRightsPlug do
   import Plug.Conn
   import Api.RouterUtils, only: [send_unauthorized: 1, get_user: 1]
-  alias Api.Core.Config
 
   def init(options), do: options
 
   def call(conn, _opts) do
-    if not is_admin(get_user(conn).user_name) do
+    if not get_user(conn).is_admin do
       conn
       |> send_unauthorized
       |> halt
     else
       conn
     end
-  end
-
-  # TODO review; anonymous user?; see also rights.ex; should the property not come from get_user now?
-  defp is_admin(user_name) do
-    users = Config.get(:rights).users
-    user  = Enum.find(users, %{}, fn user -> user.name == user_name end)
-    user[:admin] == true
   end
 end
