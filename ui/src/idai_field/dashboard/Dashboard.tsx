bf1493e9bebc8e54e683c133dbb8497752a7de24
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-02-26 14:03:17 +0100

    Add a communications box to display reports

--- ui/src/idai_field/dashboard/Dashboard.tsx
+++ ui/src/idai_field/dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import React, { CSSProperties, ReactElement, useContext, useEffect, useState } from 'react';
 import { getReadableProjects, postReindex } from '../../api/documents';
-import { NAVBAR_HEIGHT } from '../../constants';
 import { LoginContext } from '../../shared/login';
 
 
@@ -8,47 +7,103 @@ export default function Dashboard(): ReactElement {
 
     const loginData = useContext(LoginContext);
     const [projects, setProjects] = useState<string[]>([]);
-    const [stat, setStat] = useState<string>('');
+    const [stat, setStat] = useState<string[]>([]);
     
     useEffect(() => {
 
-        getReadableProjects(loginData.token).then(setProjects);
+        getReadableProjects(loginData.token).then(projects => {
+
+            const allProjects = ['All projects'].concat(projects);
+            setProjects(allProjects);
+        });
     }, [loginData]);
 
-    return (<div>
-        { stat !== '' && <div>{ stat }</div> }
+    return (<div className="container">
+        <h3 style={ headingStyle }>{ 'Dashboard' }</h3>
+        <div className="row">
         { loginData.isAdmin === true &&
-            <div style={ pageStyle }>
-                <h3 style={ headingStyle }>{ 'Dashboard' }</h3>
+            <div style={ pageStyle } className="col-md-6">
                 <p style={ paragraphStyle }>{ 'Projects'}
-                    <span className="btn"
-                    onClick={ () => triggerReindex(loginData.token, setStat, '') }>Reindex all</span>
                 </p>
-                <ul>
+                <div>
                     { projects.map((project, index) => {
-                        return <li key={ index }>{ project }
-                            <span className="btn"
-                            onClick={ () => triggerReindex(loginData.token, setStat, project) }>Reindex</span>
-                        </li>;
+                        return <div key={ index } style={ projectRowStyle }>
+                            <div style={ projectNameStyle }>
+                                { project }</div>
+                            <div style={ projectButtonsStyle }>
+                                <span className="btn" style={ rStyle }
+                                    onClick={ () => triggerReindex(loginData.token, setStat, project) }>Reindex</span>
+                            </div>
+                        </div>;
                     })}
-                </ul>
+                </div>
             </div>
-        } </div>);
+        }
+        <div className="col-md-6" style={ sideStyle }>
+            { stat.length === 0 && 'idle...' }
+            { stat.length !== 0 && stat.map((line, index) => {
+
+                return <p key={ index }>{ line }</p>;
+
+            }) }
+        </div>
+        </div></div>);
 }
 
 
 const triggerReindex = async (token: string,
-    setStat: (s: string) => void, project: string): Promise<void> => {
+    setStat: (s: string[]) => void, project: string): Promise<void> => {
 
-    const response = await postReindex(token, project);
-    setStat('response: ' + JSON.stringify(response));
+    const response = await postReindex(token, project === 'All projects' ? '' : project);
+    setStat(['Project: ' + project, 'Task: Reindex',
+        'Status:', response['status'], 'Message:', response['message']]);
+};
+
+
+const projectRowStyle: CSSProperties = {
+    height: '44px',
+    position: 'relative'
+};
+
+
+const projectNameStyle: CSSProperties = {
+    width: '200px',
+    left: '0px',
+    top: '0px',
+    height: '44px',
+    position: 'relative'
+};
+
+
+const projectButtonsStyle: CSSProperties = {
+    left: '200px',
+    top: '0px',
+    height: '44px',
+    width: 'calc(100vh - 200px)',
+    position: 'absolute',
+};
+
+
+const rStyle: CSSProperties = {
+    position: 'relative',
+    left: '0px',
+    top: '0px',
+    padding: '0',
+    color: 'green'
+};
+
+
+const sideStyle: CSSProperties = {
+
+    paddingTop: '20px',
+    fontFamily: 'monospace',
+    color: 'white',
+    backgroundColor: 'black',
+    height: 'calc(100vh - 130px)'
 };
 
 
 const pageStyle: CSSProperties = {
-    height: 'calc(100vh - ' + NAVBAR_HEIGHT + 'px)',
-    overflowY: 'scroll',
-    padding: '15px'
 };
 
 
@@ -59,6 +114,7 @@ const headingStyle: CSSProperties = {
 
 
 const paragraphStyle: CSSProperties = {
+    height: '100px',
     width: '1000px',
     marginRight: 'auto',
     marginLeft: 'auto'
