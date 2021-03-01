f59a6c2a7033b25ddff2cfd823da9fdf902f3416
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-03-01 20:47:46 +0100

    Remove dead code

--- api/lib/worker/images/conversion_controller.ex
+++ api/lib/worker/images/conversion_controller.ex
@@ -1,7 +1,6 @@
 defmodule Api.Worker.Images.ConversionController do
 
   require Logger
-  alias Api.Core.Config
   import Api.Worker.Images.ImageMagickImageConverter
 
   defp log_start_project(project) do
@@ -16,18 +15,5 @@ defmodule Api.Worker.Images.ConversionController do
       Logger.error "Sources do not exist for '#{project}'"
     end
     { :finished, project }
-  end
-
-  # TODO remove
-  def convert_images_for_all_projects do
-    projects = Config.get(:projects)
-    for project <- projects do
-      log_start_project project
-      if sources_exist? project do
-        convert_files project
-      else
-        Logger.warn "Sources do not exist for '#{project}'. Will skip image conversions for this project"
-      end
-    end
   end
 end
--- api/lib/worker/images/tiles_controller.ex
+++ api/lib/worker/images/tiles_controller.ex
@@ -3,13 +3,7 @@ defmodule Api.Worker.Images.TilesController do
   alias Api.Core.Config
   alias Api.Worker.Images.TilesCreator
 
-  def make_tiles do
-    projects = Config.get(:projects)
-    make_tiles(projects)
-  end
-  def make_tiles(projects) do
-    #IO.puts "_____ #{inspect Enprojects}"
-    Enum.map(projects, fn project -> IO.puts project end)
+  def make_tiles(projects) do # TODO pass project, instead projects
     entries = projects
     |> Enum.flat_map(&tiles_for_project/1)
     Enum.map(entries, &TilesCreator.create_tiles/1)
