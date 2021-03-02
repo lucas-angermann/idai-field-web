38ded9f90de039cceae43bc74df2bc234bfe41be
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-03-02 14:33:29 +0100

    Deduplicate

--- ui/src/api/worker.ts
+++ ui/src/api/worker.ts
@@ -4,45 +4,23 @@ import { getHeaders } from './utils';
 const PATH = '/api/worker/';
 
 
-export const postReindex = async (token: string, project: string): Promise<unknown> => {
-
-    const uri = project === ''
-        ? PATH + '/reindex'
-        : PATH + '/reindex/' + project;
-
-    const response = await fetch(uri, {
-        headers: getHeaders(token),
-        method: 'POST'
-    });
-
-    if (response.ok) return (await response.json());
-    else throw(await response.json());
-};
+export const postReindex = async (token: string, project: string): Promise<unknown> =>
+    postTask(token, 'reindex', project);
 
 
-// TODO remove duplication
-export const postConvert = async (token: string, project: string): Promise<unknown> => {
-    
-    const uri = project === '' 
-    ? PATH + '/convert'
-    : PATH + '/convert/' + project;
-    
-    const response = await fetch(uri, {
-        headers: getHeaders(token),
-        method: 'POST'
-    });
-    
-    if (response.ok) return (await response.json());
-    else throw(await response.json());
-};
+export const postConvert = async (token: string, project: string): Promise<unknown> =>
+    postTask(token, 'convert', project);
 
 
-// TODO remove duplication
-export const postTiling = async (token: string, project: string): Promise<unknown> => {
+export const postTiling = async (token: string, project: string): Promise<unknown> => 
+    postTask(token, 'tiling', project);
+
+
+export const postTask = async (token: string, endpoint: string, project: string): Promise<unknown> => {
 
     const uri = project === '' 
-        ? PATH + '/tiling'
-        : PATH + '/tiling/' + project;
+        ? PATH + '/' + endpoint
+        : PATH + '/' + endpoint + '/' + project;
 
     const response = await fetch(uri, {
         headers: getHeaders(token),
