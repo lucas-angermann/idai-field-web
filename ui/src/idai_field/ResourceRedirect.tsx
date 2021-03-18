5cbdb116f7e144e48fc49c1b6bddcd4a22cae26d
Thomas Kleinke <thomas.kleinke@dainst.de>
2021-03-18 12:18:42 +0100

    Set parent in ResourceRedirect

--- ui/src/idai_field/ResourceRedirect.tsx
+++ ui/src/idai_field/ResourceRedirect.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Redirect, useParams } from 'react-router-dom';
 import { search } from '../api/documents';
+import { ResultDocument } from '../api/result';
 import { LoginContext } from '../shared/login';
 import NotFound from '../shared/NotFound';
 
@@ -9,35 +10,41 @@ import NotFound from '../shared/NotFound';
 export default function ResourceRedirect(): ReactElement {
 
     const { project, identifier } = useParams<{ project: string, identifier: string }>();
-    const [id, setId] = useState<string>(null);
+    const [document, setDocument] = useState<ResultDocument>(null);
     const [error, setError] = useState<string>(null);
     const loginData = useContext(LoginContext);
     const { t } = useTranslation();
 
     useEffect (() => {
         
-        getId(project, identifier, loginData.token)
-            .then((resultId: string) => resultId ? setId(resultId) : setError('Not found'))
+        fetchDocument(project, identifier, loginData.token)
+            .then((result: ResultDocument) => result ? setDocument(result) : setError('Not found'))
             .catch(setError);
     }, [project, identifier, loginData]);
 
     return error
         ? <NotFound />
-        : id
-            ? <Redirect to={ `/project/${project}/hierarchy/${id}` } />
+        : document
+            ? <Redirect to={ `/project/${project}/hierarchy/${document.resource.id}?parent=${getParent(document)}` } />
             : <div>{ t('redirect.waitForRedirection')}</div>;
 }
 
 
-const getId = async (project: string, identifier: string, token: string): Promise<string> => {
+const fetchDocument = async (project: string, identifier: string, token: string): Promise<ResultDocument> => {
 
     const result = await search({ filters: getFilters(project, identifier), q: '' }, token);
 
     if (result.documents.length > 0) {
-        return result.documents[0].resource.id;
+        return result.documents[0];
     } else {
         throw new Error('Not found');
     }
+};
+
+
+const getParent = (document: ResultDocument): string => {
+
+    return document.resource.parentId || 'root';
 };
 
 
